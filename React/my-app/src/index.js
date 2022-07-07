85aa68ed8c8ed1d52b842794781926f69d040507
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2022-07-07 20:01:24 -0300

    up

--- React/my-app/src/index.js
+++ React/my-app/src/index.js
@@ -1,11 +1,19 @@
 import React from 'react'
 import ReactDom from 'react-dom'
+import Filho from './componentes/Filho'
 //import { BomDia, BoaNoite } from './componentes/Multiplos'
-//clsimport Multi from './componentes/Multiplos'
-import Saudacao from './componentes/Saudacao'
+//import Multi from './componentes/Multiplos'
+//import Saudacao from './componentes/Saudacao'
+import Pai from './componentes/Pai'
 
 ReactDom.render(
     <div>
-        <Saudacao tipo='Boa noite' nome='Cleber'/>
+       {/*<Saudacao tipo='Boa noite' nome='Cleber'/>*/}
+        <Pai nome='Paulo' sobrenome='Silva'>
+            <Filho nome='Pedro' />
+            <Filho nome='Paulo' />
+            <Filho nome='Carla' />
+        </Pai>
+
     </div>
     , document.getElementById('root'))
