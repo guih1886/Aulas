0996077cd106a2f92bca99de1e19e80ff095db5e
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2022-07-21 18:42:48 -0300

    up

--- NextJS/fundamentos/src/pages/index.jsx
+++ NextJS/fundamentos/src/pages/index.jsx
@@ -4,12 +4,13 @@ import Navegador from '../components/Navegador'
 export default function Estiloso() {
     return (
         <div>
-            <Navegador texto="Estiloso" destino='/estiloso' color='blue'></Navegador><br />
+            <Navegador texto="Estiloso" destino='/estiloso'></Navegador>
             <Navegador texto="JSX" destino='/jsx'></Navegador>
             <Navegador texto="Exemplo" destino='/exemplo'></Navegador>
             <Navegador texto="Navagação #1" destino='/cliente/123'></Navegador>
             <Navegador texto="Componente com Estado" destino='/estado'></Navegador>
             <Navegador texto="Integração com API #01" destino='/integracao'></Navegador>
+            <Navegador texto="Conteúdo Estático" destino='/estatico'></Navegador>
         </div>
     )
 }
