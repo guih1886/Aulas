16d0b1265c02ace6ddeb98e2241eb7cf3a61e672
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2022-10-22 19:02:10 -0300

    react native
    up

--- React_Native/TargetSum/src/components/Game.js
+++ React_Native/TargetSum/src/components/Game.js
@@ -1,28 +1,39 @@
 import React, { Component } from 'react'
 import { View, Text, StyleSheet } from 'react-native'
 import PropTypes from 'prop-types'
+import RandomNumber from './RandomNumber'
 
 export default class Game extends Component {
     static propTypes = {
         randomNumberCount: PropTypes.number.isRequired
     }
-    //define the quandidy of numbers for the sum
+    state = {
+        selectedNumbers: [],
+    }
+    //define the quantity of numbers for the sum
     randomNumbers = Array.from({ length: this.props.randomNumberCount }).map(() => 1 + Math.floor(10 * Math.random()))
-
     target = this.randomNumbers.slice(0, this.props.randomNumberCount - 2).reduce((acc, curr) => acc + curr, 0)
-
+    isNumberSelected = (numberIndex) => {
+        return this.state.selectedNumbers.indexOf(numberIndex) >= 0
+    }
+    selectNumber = (numberIndex) => {
+        this.setState((prevState) => ({
+            selectedNumbers: [...prevState.selectedNumbers, numberIndex]
+        }))
+    }
     render() {
         return (
             <View style={styles.container}>
                 <Text style={styles.target}>{this.target}</Text>
                 <View style={styles.randomContainer}>
-                    {this.randomNumbers.map((randomNumber, index) => <Text style={styles.random} key={index}>{randomNumber}</Text>)}
+                    {this.randomNumbers.map((randomNumber, index) =>
+                        <RandomNumber key={index} id={index} number={randomNumber} isDisabled={this.isNumberSelected(index)} onPress={this.selectNumber}/>)
+                    }
                 </View>
             </View>
         )
     }
 }
-
 const styles = StyleSheet.create({
     container: {
         backgroundColor: 'orange',
@@ -34,14 +45,13 @@ const styles = StyleSheet.create({
         color: '#fff',
         backgroundColor: 'gray',
         marginHorizontal: 50,
-        textAlign: 'center'
+        textAlign: 'center',
+        marginBottom: 50
     },
     randomContainer: {
-        backgroundColor: '#444',
-        flexDirection: 'row'
-        
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        flex: 1,
+        justifyContent: 'space-around'
     },
-    random: {
-
-    }
 })
---
+++ React_Native/TargetSum/src/components/RandomNumber.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react'
+import { Text, StyleSheet, TouchableOpacity } from 'react-native'
+import PropTypes from 'prop-types'
+
+export default class RandomNumber extends Component {
+    static propTypes = {
+        id: PropTypes.number.isRequired,
+        number: PropTypes.number.isRequired,
+        isDisabled: PropTypes.bool.isRequired,
+        onPress: PropTypes.func.isRequired,
+    }
+
+    handlePress = () => {
+       this.props.onPress(this.props.id)
+    }
+
+    render() {
+        return (
+            <TouchableOpacity onPress={this.handlePress}>
+                <Text style={[styles.random, this.props.isDisabled && styles.disabled]}>{this.props.number}</Text>
+            </TouchableOpacity>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    random: {
+        backgroundColor: '#555',
+        width: 100,
+        textAlign: 'center',
+        marginHorizontal: 15,
+        marginVertical: 25,
+        fontSize: 30,
+    },
+    disabled: {
+        opacity: 0.5
+    }
+})
