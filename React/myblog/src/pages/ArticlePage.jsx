296d0a797ce5562735a89003757fa0ee34fbf107
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2022-11-30 22:41:18 -0300

    react update
    react update

---
+++ React/myblog/src/pages/ArticlePage.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { useParams } from 'react-router-dom'
+import articles from './article-content'
+import NotFound from './NotFoundPage';
+
+export default function ArticlePage() {
+    const { articleId } = useParams();
+    const article = articles.find(article => article.name === articleId)
+
+    if (!article) {
+        return <NotFound />
+    }
+
+    return (
+        <>
+            <h1>{article.title}</h1>
+            {article.content.map((paragraph, i) => (
+                <p key={i}>{paragraph}</p>
+            ))}
+        </>
+    )
+}
