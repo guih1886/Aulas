6293552b050981e0e69b0df99a62683c144d6bdb
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2023-03-08 14:32:08 -0300

    java class update
    java class update

---
+++ Java/ListaExerciciosJava/Ex2/app/src/main/java/Ex2/App.java
@@ -0,0 +1,29 @@
+package Ex2;
+
+import java.util.Scanner;
+
+public class App {
+
+    public static void main(String[] args) {
+        float n1;
+        float n2;
+        float soma;
+        float subtracao;
+        float multiplicacao;
+        float divisao;
+        Scanner leitor = new Scanner(System.in);
+
+        System.out.print("Digite o numero 1: ");
+        n1 = leitor.nextInt();
+        System.out.print("Digite o numero 2: ");
+        n2 = leitor.nextInt();
+        soma = n1 + n2;
+        System.out.println("Soma: " + soma);
+        subtracao = n1 - n2;
+        System.out.println("Subtracao: " + subtracao);
+        multiplicacao = n1 * n2;
+        System.out.println("Multiplicacao: " + multiplicacao);
+        divisao = n1 / n2;
+        System.out.println("Divisao: " + divisao);
+    }
+}
