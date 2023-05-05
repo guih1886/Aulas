8daaf71aa664de1fe09a442806509c15f19e1c5b
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2023-05-05 19:52:06 -0300

    java spring update
    java spring update

---
+++ Java - Spring boot/api/src/main/java/med/voll/api/domain/consulta/DadosAgendamentoConsulta.java
@@ -0,0 +1,15 @@
+package med.voll.api.domain.consulta;
+
+import java.time.LocalDateTime;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.NotNull;
+
+public record DadosAgendamentoConsulta(
+                Long idMedico,
+                @NotNull Long idPaciente,
+                @NotNull @Future @JsonFormat(pattern = "dd/MM/yyyy HH:mm") LocalDateTime data) {
+
+}
