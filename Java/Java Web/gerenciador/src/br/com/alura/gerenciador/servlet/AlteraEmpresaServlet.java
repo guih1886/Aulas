a22b3e331e713c1eecfc7502a02522c43a213f47
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2023-05-12 20:32:54 -0300

    java web update
    java web update

--- Java/Java Web/gerenciador/src/br/com/alura/gerenciador/servlet/AlteraEmpresaServlet.java
+++ Java/Java Web/gerenciador/src/br/com/alura/gerenciador/servlet/AlteraEmpresaServlet.java
@@ -1,15 +1,13 @@
 package br.com.alura.gerenciador.servlet;
 
-import java.io.IOException;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import br.com.alura.gerenciador.modelo.Banco;
+import br.com.alura.gerenciador.modelo.Empresa;
 
 @WebServlet("/alteraEmpresa")
 public class AlteraEmpresaServlet extends HttpServlet {
