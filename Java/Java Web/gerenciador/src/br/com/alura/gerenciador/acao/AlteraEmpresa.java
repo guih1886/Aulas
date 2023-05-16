9911f663c71b6e4b977eca415f10a5497ee0b58b
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2023-05-15 22:10:17 -0300

    java web update
    java web update

--- Java/Java Web/gerenciador/src/br/com/alura/gerenciador/acao/AlteraEmpresa.java
+++ Java/Java Web/gerenciador/src/br/com/alura/gerenciador/acao/AlteraEmpresa.java
@@ -1,27 +1,28 @@
-package br.com.alura.gerenciador.servlet;
+package br.com.alura.gerenciador.acao;
+
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import br.com.alura.gerenciador.modelo.Banco;
 import br.com.alura.gerenciador.modelo.Empresa;
 
-@WebServlet("/alteraEmpresa")
-public class AlteraEmpresaServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+public class AlteraEmpresa {
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public void executa(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
 		System.out.println("Alterando empresa");
-		
+
 		String nomeEmpresa = request.getParameter("nome");
 		String paramDataEmpresa = request.getParameter("data");
 		String paramId = request.getParameter("id");
 		Integer id = Integer.valueOf(paramId);
-		
+
 		Date dataAbertura = null;
 		try {
 			SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
@@ -29,16 +30,15 @@ public class AlteraEmpresaServlet extends HttpServlet {
 		} catch (ParseException e) {
 			throw new ServletException(e);
 		}
-		
+
 		System.out.println(id);
-		
+
 		Banco banco = new Banco();
 		Empresa empresa = banco.buscaEmpresaPelaId(id);
 		empresa.setNome(nomeEmpresa);
 		empresa.setDataAbertura(dataAbertura);
-		
-		response.sendRedirect("listaEmpresas");
-	
+
+		response.sendRedirect("entrada?acao=ListaEmpresas");
 	}
 
 }
