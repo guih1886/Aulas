831521f0057d9492065e3734c1041d949429429f
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2023-05-16 22:11:54 -0300

    java web update
    java web update

--- Java/Java Web/gerenciador/src/br/com/alura/gerenciador/modelo/Banco.java
+++ Java/Java Web/gerenciador/src/br/com/alura/gerenciador/modelo/Banco.java
@@ -5,16 +5,16 @@ import java.util.Iterator;
 import java.util.List;
 
 public class Banco {
-	
+
 	private static List<Empresa> lista = new ArrayList<>();
 	private static Integer chaveSequencial = 1;
-	
+
 	static {
 		Empresa empresa = new Empresa();
 		empresa.setId(chaveSequencial++);
 		empresa.setNome("Alura");
 		Empresa empresa2 = new Empresa();
-		empresa2.setId(chaveSequencial++);
+		Passaro pass = new empresa2.setId(chaveSequencial++);
 		empresa2.setNome("Caelum");
 		lista.add(empresa);
 		lista.add(empresa2);
@@ -24,19 +24,19 @@ public class Banco {
 		empresa.setId(Banco.chaveSequencial++);
 		Banco.lista.add(empresa);
 	}
-	
-	public List<Empresa> getEmpresas(){
+
+	public List<Empresa> getEmpresas() {
 		return Banco.lista;
 	}
 
 	public void removeEmpresa(Integer id) {
-		
+
 		Iterator<Empresa> it = lista.iterator();
-		
-		while(it.hasNext()) {
+
+		while (it.hasNext()) {
 			Empresa emp = it.next();
-			
-			if(emp.getId() == id) {
+
+			if (emp.getId() == id) {
 				it.remove();
 			}
 		}
@@ -44,7 +44,7 @@ public class Banco {
 
 	public Empresa buscaEmpresaPelaId(Integer id) {
 		for (Empresa empresa : lista) {
-			if(empresa.getId() == id) {
+			if (empresa.getId() == id) {
 				return empresa;
 			}
 		}
