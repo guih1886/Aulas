ce3db6203596565dd726f824f3d4f0a3cf09ed67
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2023-06-02 21:52:53 -0300

    project start
    project start

---
+++ TypeScript/event-tracker/src/components/Evento/EventoCheckbox/index.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { IEvento } from '../../../interfaces/IEvento';
+import { useSetRecoilState } from 'recoil';
+import { listaDeEventosState } from '../../../state/atom';
+
+const EventoCheckbox: React.FC<{ evento: IEvento }> = ({ evento }) => {
+
+  const setListaDeEventos = useSetRecoilState<IEvento[]>(listaDeEventosState);
+
+  const estilos = [
+    'far',
+    'fa-2x',
+    evento.completo ? 'fa-check-square' : 'fa-square'
+  ]
+
+  const alterarStatus = () => {
+    const eventoAlterado = {
+      ...evento
+    }
+    eventoAlterado.completo = !eventoAlterado.completo
+
+    setListaDeEventos(listaAntiga => {
+      const indice = listaAntiga.findIndex(evt => evt.id === evento.id)
+      return [...listaAntiga.slice(0, indice), eventoAlterado, ...listaAntiga.slice(indice + 1)]
+    })
+
+  }
+
+  return (<i className={estilos.join(' ')} onClick={alterarStatus}></i>)
+}
+
+export default EventoCheckbox
---
+++ TypeScript/event-tracker/src/components/ListaDeEventos/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Evento from '../Evento';
+import Filtro from '../Filtro';
+import style from './ListaDeEventos.module.scss';
+import { useRecoilValue } from 'recoil'
+import { listaDeEventosState } from '../../state/atom';
+
+const ListaDeEventos: React.FC<{
+  aoDeletarEvento: (id: number) => void,
+  aoFiltroAplicado: (data: Date | null) => void
+}> = ({ aoDeletarEvento, aoFiltroAplicado }) => {
+
+  const eventos = useRecoilValue(listaDeEventosState);
+
+  return (<section>
+    <Filtro aoFiltroAplicado={aoFiltroAplicado} />
+    <div className={style.Scroll}>
+      {eventos.map(evento => (
+        <Evento evento={evento} key={evento.id} />
+      ))}
+    </div>
+  </section>)
+}
+
+export default ListaDeEventos
