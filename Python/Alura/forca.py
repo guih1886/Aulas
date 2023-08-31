9d4b545ab9bef1288224029abe70c623739f94d4
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2023-08-30 21:24:53 -0300

    python class update
    python class update

--- Python/Alura/forca.py
+++ Python/Alura/forca.py
@@ -1,18 +1,61 @@
+import random
+
+
 def jogar():
+
+    imprime_mensagem_abertura()
+
+    palavra_secreta = carrega_palavra_secreta()
+
+    letras_acertadas = inicializa_letras_acertadas(palavra_secreta)
+
+    enforcou = False
+    acertou = False
+    erros = 0
+
+    while (not enforcou and not acertou):
+        chute = input("Qual letra?: ")
+        chute = chute.strip().upper()
+
+        index = 0
+        for letra in palavra_secreta:
+            if letra == chute:
+                letras_acertadas[index] = letra
+            index += 1
+        erros += 1
+
+        print(letras_acertadas, "\n")
+        enforcou = erros == 6
+        acertou = "_" not in letras_acertadas
+
+    if acertou:
+        print("Você ganhou")
+    else:
+        print("Fim do jogo")
+
+
+def imprime_mensagem_abertura():
     print(25*"*")
     print("Bem vindo ao jogo da forca")
     print(25*"*")
 
-    palavra_secreta = "banana"
-    enforcou = False
-    acertou = False
 
-    while (not enforcou and not acertou):
-        chute = input("Qual letra?: ")
-        palavra_secreta.find(chute)
-        print("jogando...")
+def carrega_palavra_secreta():
+    palavras = []
 
-    print("Fim do jogo")
+    with open("C:\\Users\\guilh\\Documents\\GitHub\\Aulas\\Python\\Alura\\palavras.txt", "r") as arquivo:
+        for linha in arquivo:
+            palavras.append(linha.strip())
+
+    numero = random.randrange(0, len(palavras))
+
+    palavra_secreta = palavras[numero].upper()
+
+    return palavra_secreta
+
+
+def inicializa_letras_acertadas(palavra):
+    return ["_" for letra in palavra]
 
 
 if (__name__ == '__main__'):
