82edde985ef6dba2145bc0d80ca3bc82315b48ae
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2023-09-01 20:42:36 -0300

    python class update
    python class update

--- Python/Alura/oo/Conta.py
+++ Python/Alura/oo/Conta.py
@@ -1,22 +1,55 @@
 class Conta:
     def __init__(self, numero, titular, saldo, limite):
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+        self.__numero = numero  # encapsulamento é feito com __ (private)
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
+        self.__codigo_banco = "001"
 
     def extrato(self):
-        print("Saldo {} do titular {}.".format(self.saldo, self.titular))
+        print("Saldo {} do titular {}.".format(self.__saldo, self.__titular))
+
+    def __pode_sacar(self, valor):
+        return valor > self.__saldo + self.__limite
 
     def sacar(self, valor):
-        if valor > self.saldo:
+        if self.__pode_sacar(valor):
             print("Saldo insuficiente.")
         else:
-            self.saldo -= valor
+            self.__saldo -= valor
+            print("Saque de {} do titular {} efetuado com sucesso.".format(valor, self.__titular))
 
     def depositar(self, valor):
         if valor <= 0:
             print("Valor inválido, informe um depósito maior que zero.")
         else:
-            self.saldo += valor
-            print("Depositado o valor {} na conta número {} de {}".format(valor, self.numero, self.titular))
+            self.__saldo += valor
+            print("Depositado o valor {} na conta número {} de {}".format(valor, self.__numero, self.__titular))
+
+    def transferir(self, valor, destino):
+        if valor <= self.__saldo:
+            self.sacar(valor)
+            destino.depositar(valor)
+            print("Transferência efetuada com sucesso.")
+        else:
+            print("Saldo insuficiente.")
+
+    @property  # define uma propriedade, age como um getter
+    def saldo(self):
+        return self.__saldo
+
+    @property
+    def titular(self):
+        return self.__titular
+
+    @property
+    def limite(self):
+        return self.__limite
+
+    @limite.setter  # age como se fosse um setter, mas o property correspondente tem que estar setado
+    def limite(self, novo_limite):
+        self.__limite = novo_limite
+
+    @staticmethod  # notação para tornar um método estático (static)
+    def codigo_banco():
+        return "001"
--- Python/Alura/oo/main.py
+++ Python/Alura/oo/main.py
@@ -1,8 +1,11 @@
 from Conta import *
 
 conta1 = Conta(123, "Guilherme", 1000, 50000)
+conta2 = Conta(124, "Pedro", 2000, 55000)
 
-conta1.depositar(1)
-conta1.sacar(1800)
+#conta1.depositar(1)
+#conta1.sacar(1800)
 
-print(conta1.extrato())
+conta1.transferir(500, conta2)
+conta2.limite = 5000
+print(conta2.limite)
