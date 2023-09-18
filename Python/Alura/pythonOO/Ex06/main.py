0b41fb640dd4a9ec2b8c25e3072a6409f25574b4
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2023-09-18 18:26:06 -0300

    python exercises update
    python exercises update

---
+++ Python/Alura/pythonOO/Ex06/main.py
@@ -0,0 +1,4 @@
+# Exercicio Invoice, simulador de loja. Com os atributos do número do item da fa-
+# tura, descrição do item, quantidade e preço unitario. O método getInvoiceAmount
+# retorna o valor da compra.
+
