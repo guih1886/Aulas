25af5e6abf0d97987d6aaa9ffa3b33d91604f57d
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2023-10-12 08:56:32 -0300

    django class api-escola creation
    django class api-escola creation

---
+++ Frameworks/Django/api-escola/escola/views.py
@@ -0,0 +1,7 @@
+from django.http import JsonResponse
+
+
+def alunos(request):
+    if request.method == 'GET':
+        alunos = {'id': 1, 'nome': 'Guilherme'}
+        return JsonResponse(alunos)
---
+++ Frameworks/Django/api-escola/setup/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path, include
+from escola.views import alunos
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('alunos', alunos),
+    path('api-auth/', include('rest_framework.urls'))
+]
