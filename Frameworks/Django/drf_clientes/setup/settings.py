7de9d3e614c62e25260096817e33e28bb2e30250
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2023-10-16 20:05:10 -0300

    django rest project update
    django rest project update

--- Frameworks/Django/drf_clientes/setup/settings.py
+++ Frameworks/Django/drf_clientes/setup/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'clientes'
+    'clientes',
+    'django-filters',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +122,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Paginação para o rest framework
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 5
+}
