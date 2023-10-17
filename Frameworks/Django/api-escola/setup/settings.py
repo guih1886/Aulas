688c41e6b8bed85e25c383a15d4bb7ac5f4e1b13
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2023-10-17 17:11:33 -0300

    django rest api-escola throttle update
    django rest api-escola throttle update

--- Frameworks/Django/api-escola/setup/settings.py
+++ Frameworks/Django/api-escola/setup/settings.py
@@ -132,5 +132,13 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.BasicAuthentication',
-    ]
+    ],
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle'
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '100/day',
+        'user': '10/minute'
+    }
 }
