eecd56a4b3cba39d96f6b6f7d067a4a93e2c765d
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2023-10-20 15:07:09 -0300

    Update settings.py

--- Frameworks/Django/aluraflix/setup/settings.py
+++ Frameworks/Django/aluraflix/setup/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--!wx_++t=3wf)$xe(bd3l(b86jil9bjdmlql(l1*abm6*4vn-w'
+SECRET_KEY = os.path.join('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
