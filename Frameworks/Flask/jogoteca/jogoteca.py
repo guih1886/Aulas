8ed1c0d4967fc9043ec6d88348e9e21e3b408399
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2023-10-26 19:43:46 -0300

    flask project update
    flask project update

--- Frameworks/Flask/jogoteca/jogoteca.py
+++ Frameworks/Flask/jogoteca/jogoteca.py
@@ -1,10 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
-app.config.from_pyfile('config.py')
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
 
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 from views import *
 
