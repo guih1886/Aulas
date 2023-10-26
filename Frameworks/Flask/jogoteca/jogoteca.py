98027937fd6365ae91f45f0c468d5ebab9c80e7f
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2023-10-26 20:18:41 -0300

    flask project update
    flask project update

--- Frameworks/Flask/jogoteca/jogoteca.py
+++ Frameworks/Flask/jogoteca/jogoteca.py
@@ -1,13 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
+from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
-
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+app.config.from_pyfile('config.py')
 
 db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+csrf = CSRFProtect(app)
 
 from views import *
 
