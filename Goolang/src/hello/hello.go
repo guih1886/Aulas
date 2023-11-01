d6117c45442ca03227c09b9a1bcbcf77440baf89
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2023-10-31 21:34:09 -0300

    Update hello.go

--- Goolang/src/hello/hello.go
+++ Goolang/src/hello/hello.go
@@ -2,17 +2,42 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func main() {
 	/* := serve para omitir a declaração de var, o go possui inferencia de tipos*/
+
+	exibeIntroducao()
+	exibeMenu()
+	comando := leComando()
+
+	switch comando {
+	case 1:
+		fmt.Println("Monitorando...")
+	case 2:
+		fmt.Println("Exibindo Logs...")
+	case 0:
+		fmt.Println("Saindo...")
+		os.Exit(0)
+	default:
+		fmt.Println("Comando inválido.")
+	}
+}
+
+func exibeIntroducao() {
 	nome := "Guilherme"
 	fmt.Println("Olá", nome)
+}
 
+func exibeMenu() {
 	fmt.Println("1- Iniciar Monitoramento")
 	fmt.Println("2- Exibir Logs")
 	fmt.Println("0- Sair do Programa")
-	var comando int
+}
 
+func leComando() int {
+	var comando int
 	fmt.Scan(&comando)
+	return comando
 }
