4b97c19116db217577dfdcde565c85ea674ebf0f
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2023-11-07 20:21:01 -0300

    go class update
    go class update

--- Goolang/src/hello/hello.go
+++ Goolang/src/hello/hello.go
@@ -1,27 +1,37 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
+	"net/http"
 	"os"
+	"strings"
+	"time"
 )
+
+const monitoramentos = 10
+const delay = 5
 
 func main() {
 	/* := serve para omitir a declaração de var, o go possui inferencia de tipos*/
 
 	exibeIntroducao()
-	exibeMenu()
-	comando := leComando()
+	for {
+		exibeMenu()
+		comando := leComando()
 
-	switch comando {
-	case 1:
-		fmt.Println("Monitorando...")
-	case 2:
-		fmt.Println("Exibindo Logs...")
-	case 0:
-		fmt.Println("Saindo...")
-		os.Exit(0)
-	default:
-		fmt.Println("Comando inválido.")
+		switch comando {
+		case 1:
+			iniciarmonitoramento()
+		case 2:
+			fmt.Println("Exibindo Logs...")
+		case 0:
+			fmt.Println("Saindo...")
+			os.Exit(0)
+		default:
+			fmt.Println("Comando inválido.")
+		}
 	}
 }
 
@@ -40,4 +50,51 @@ func leComando() int {
 	var comando int
 	fmt.Scan(&comando)
 	return comando
+}
+
+func iniciarmonitoramento() {
+	fmt.Println("Monitorando...")
+	sites := lerArquivo()
+
+	for i := 0; i < monitoramentos; i++ {
+		for _, site := range sites {
+			testaSite(site)
+		}
+		time.Sleep(delay * time.Second)
+	}
+
+}
+
+func testaSite(site string) {
+	resp, _ := http.Get(site)
+	if resp.StatusCode == 200 {
+		fmt.Println("Site:", site, "foi carregado com sucesso!")
+	} else {
+		fmt.Println("Site:", site, "está com problemas. Status Code:", resp.StatusCode)
+	}
+}
+
+func lerArquivo() []string {
+	var sites []string
+
+	arquivo, err := os.Open("sites.txt")
+
+	if err != nil {
+		fmt.Println("Ocorreu um erro:", err)
+	}
+
+	leitor := bufio.NewReader(arquivo)
+
+	for {
+		linha, err := leitor.ReadString('\n')
+		linha = strings.TrimSpace(linha)
+
+		sites = append(sites, linha)
+
+		if err == io.EOF {
+			break
+		}
+	}
+
+	return sites
 }
