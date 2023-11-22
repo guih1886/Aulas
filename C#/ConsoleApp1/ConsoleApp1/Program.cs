293ece39c6d64beff6aef1b2ff28fbb824974cb8
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2023-11-22 20:44:03 -0300

    C# class
    C# class

---
+++ C#/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,79 @@
+﻿// Screen Sound
+List<string> bandas = new List<string>();
+
+void ExibirLogo()
+{
+    Console.WriteLine(@"
+    ░██████╗░█████╗░██████╗░███████╗███████╗███╗░░██╗  ░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░
+    ██╔════╝██╔══██╗██╔══██╗██╔════╝██╔════╝████╗░██║  ██╔════╝██╔══██╗██║░░░██║████╗░██║██╔══██╗
+    ╚█████╗░██║░░╚═╝██████╔╝█████╗░░█████╗░░██╔██╗██║  ╚█████╗░██║░░██║██║░░░██║██╔██╗██║██║░░██║
+    ░╚═══██╗██║░░██╗██╔══██╗██╔══╝░░██╔══╝░░██║╚████║  ░╚═══██╗██║░░██║██║░░░██║██║╚████║██║░░██║
+    ██████╔╝╚█████╔╝██║░░██║███████╗███████╗██║░╚███║  ██████╔╝╚█████╔╝╚██████╔╝██║░╚███║██████╔╝
+    ╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝  ╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░
+    ");
+}
+
+void ExibirOpcoesDoMenu()
+{
+    Console.Clear();
+    ExibirLogo();
+    Console.WriteLine("Digite 1 para registrar uma banda");
+    Console.WriteLine("Digite 2 para mostrar todas as bandas");
+    Console.WriteLine("Digite 3 para avaliar uma banda");
+    Console.WriteLine("Digite 4 para exibir a média de uma banda");
+    Console.WriteLine("Digite -1 para sair");
+
+    Console.Write("Digite sua opção: ");
+    string opcao = Console.ReadLine();
+    int opcaoInt = int.Parse(opcao);
+    switch (opcaoInt)
+    {
+        case 1:
+            RegistrarBanda();
+            break;
+        case 2:
+            ExibirBandas();
+            break;
+        case 3:
+            Console.WriteLine();
+            break;
+        case 4:
+            Console.WriteLine();
+            break;
+        case -1:
+            Console.WriteLine("Saindo...");
+            break;
+        default:
+            Console.WriteLine("Opção inválida.");
+            break;
+    }
+
+}
+
+void RegistrarBanda()
+{
+    Console.Clear();
+    Console.WriteLine("Registro de bandas");
+    Console.Write("Digite o nome da banda: ");
+    string nomeDaBanda = Console.ReadLine()!;
+    bandas.Add(nomeDaBanda);
+    Console.WriteLine($"Banda {nomeDaBanda} cadastrada com sucesso!");
+    Thread.Sleep(2000);
+    Console.Clear();
+    ExibirOpcoesDoMenu();
+}
+
+void ExibirBandas()
+{
+    Console.Clear();
+    Console.WriteLine("Exibindo as bandas cadastradas");
+    foreach (string item in bandas)
+    {
+        Console.WriteLine("Banda: " + item);
+    }
+    Console.ReadKey();
+    ExibirOpcoesDoMenu();
+}
+
+ExibirOpcoesDoMenu();
+Console.ReadLine();
