25ae0b2ff7df258129a919e515cc235593686744
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2023-11-23 19:24:50 -0300

    C# POO class project creation
    C# POO class project creation

---
+++ C#/ScreenSound/ScreenSound/Musica.cs
@@ -0,0 +1,30 @@
+﻿
+class Musica
+{
+    private string nome;
+    private string artista;
+    private int duracao;
+    private bool disponivel;
+
+    public Musica(string nome, string artista, int duracao, bool disponivel)
+    {
+        this.nome = nome;
+        this.artista = artista;
+        this.duracao = duracao;
+        this.disponivel = disponivel;
+    }
+
+    public void ExibirFichaTecnica()
+    {
+        Console.WriteLine($"Nome: {this.nome} - Artista: {this.artista} - Duração {this.duracao}s ");
+        if (disponivel)
+        {
+            Console.WriteLine("Disponível no plano.");
+        }
+        else
+        {
+            Console.WriteLine("Disponível no plano premium.");
+        }
+    }
+}
+
---
+++ C#/ScreenSound/ScreenSound/main.cs
@@ -0,0 +1,5 @@
+﻿Musica musica1 = new Musica("Musica1", "Artista1", 120, true);
+Musica musica2 = new Musica("Musica2", "Artista2", 240, false);
+
+musica1.ExibirFichaTecnica();
+musica2.ExibirFichaTecnica();
