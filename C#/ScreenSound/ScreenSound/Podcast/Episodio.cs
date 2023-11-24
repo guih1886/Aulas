4a636edac11b6751ef965d72f8187d6d16dbd499
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2023-11-24 20:04:26 -0300

    package podcast creation
    package podcast creation

---
+++ C#/ScreenSound/ScreenSound/Podcast/Episodio.cs
@@ -0,0 +1,13 @@
+﻿class Episodio(int ordem, string titulo, int duracao)
+{
+    private List<string> convidados = new List<string>();
+    public int Ordem { get; } = ordem;
+    private string Titulo { get; } = titulo;
+    private int Duracao { get; } = duracao;
+    public string Resumo => $"{Ordem} - {Titulo} ({Duracao}s) - {string.Join(", ", convidados)}";
+
+    public void AdicionarConvidados(string convidado)
+    {
+        convidados.Add(convidado);
+    }
+}
