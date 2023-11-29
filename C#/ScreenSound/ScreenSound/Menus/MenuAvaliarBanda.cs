179da23dea75b87af2207e4d0523b3594b35915c
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2023-11-29 20:50:42 -0300

    Create MenuAvaliarBanda.cs

---
+++ C#/ScreenSound/ScreenSound/Menus/MenuAvaliarBanda.cs
@@ -0,0 +1,11 @@
+﻿using ScreenSound.Modelos;
+
+namespace ScreenSound.Menus;
+
+internal class MenuAvaliarBanda : Menu
+{
+    public override void Executar(Dictionary<string, Banda> bandasRegistradas)
+    {
+
+    }
+}
