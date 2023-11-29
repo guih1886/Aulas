c60a2eca0a2866b26a5c28f712c671e597272180
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2023-11-29 20:50:55 -0300

    Create MenuRegistrarBanda.cs

---
+++ C#/ScreenSound/ScreenSound/Menus/MenuRegistrarBanda.cs
@@ -0,0 +1,16 @@
+﻿using OpenAI_API;
+using ScreenSound.Menus;
+using ScreenSound.Modelos;
+
+internal class MenuRegistrarBanda : Menu
+{
+    public override void Executar(Dictionary<string, Banda> bandasRegistradas)
+    {
+        base.Executar(bandasRegistradas);
+        //integração com o chatgpt
+        var client = new OpenAIAPI("chave");
+        var chat = client.Chat.CreateConversation();
+        chat.AppendSystemMessage("");
+        string resposta = chat.GetResponseFromChatbotAsync().GetAwaiter().GetResult();
+    }
+}
