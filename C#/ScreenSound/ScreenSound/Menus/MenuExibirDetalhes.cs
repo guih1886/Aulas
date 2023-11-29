dd28fca119ccad923565a71f90afce37f2b5bd40
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2023-11-29 20:50:46 -0300

    Create MenuExibirDetalhes.cs

---
+++ C#/ScreenSound/ScreenSound/Menus/MenuExibirDetalhes.cs
@@ -0,0 +1,11 @@
+﻿using ScreenSound.Modelos;
+
+namespace ScreenSound.Menus;
+
+internal class MenuExibirDetalhes : Menu
+{
+    public override void Executar(Dictionary<string, Banda> bandasRegistradas)
+    {
+        base.Executar(bandasRegistradas);
+    }
+}
