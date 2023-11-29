3423c7aa553ca0c0183aef51b8954bed9d0cad30
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2023-11-29 20:50:52 -0300

    Create MenuRegistrarAlbum.cs

---
+++ C#/ScreenSound/ScreenSound/Menus/MenuRegistrarAlbum.cs
@@ -0,0 +1,12 @@
+﻿using OpenAI_API;
+using ScreenSound.Menus;
+using ScreenSound.Modelos;
+
+internal class MenuRegistrarAlbum : Menu
+{
+    public override void Executar(Dictionary<string, Banda> bandasRegistradas)
+    {
+        base.Executar(bandasRegistradas);
+        
+    }
+}
