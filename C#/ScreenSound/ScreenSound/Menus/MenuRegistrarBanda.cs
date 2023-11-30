47089fd13e2876b6b3a264e28276bcf6367a319b
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2023-11-30 08:37:55 -0300

    Update MenuRegistrarBanda.cs

--- C#/ScreenSound/ScreenSound/Menus/MenuRegistrarBanda.cs
+++ C#/ScreenSound/ScreenSound/Menus/MenuRegistrarBanda.cs
@@ -7,10 +7,21 @@ internal class MenuRegistrarBanda : Menu
     public override void Executar(Dictionary<string, Banda> bandasRegistradas)
     {
         base.Executar(bandasRegistradas);
-        //integração com o chatgpt
-        var client = new OpenAIAPI("chave");
+        ExibirTituloOpcao("Registro das bandas");
+        Console.Write("Digite o nome da banda que deseja registrar: ");
+        string nomeDaBanda = Console.ReadLine()!;
+        Banda banda = new Banda(nomeDaBanda);
+        bandasRegistradas.Add(nomeDaBanda, banda);
+
+        var client = new OpenAIAPI("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");
         var chat = client.Chat.CreateConversation();
-        chat.AppendSystemMessage("");
-        string resposta = chat.GetResponseFromChatbotAsync().GetAwaiter().GetResult();
+        chat.AppendSystemMessage($"Resuma a banda {nomeDaBanda} em 1 parágrafo. Adote um estilo informal");
+        var resposta = chat.GetResponseFromChatbotAsync().GetAwaiter().GetResult();
+        banda.Resumo = resposta;
+
+        Console.WriteLine($"A banda {nomeDaBanda} foi registrada com sucesso!");
+        Console.WriteLine("Digite uma tecla para voltar ao menu principal");
+        Console.ReadKey();
+        Console.Clear();
     }
 }
