e436ccc77113a033a819471a0d04750f0b3637ad
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2023-11-30 08:37:48 -0300

    Update Menu.cs

--- C#/ScreenSound/ScreenSound/Menus/Menu.cs
+++ C#/ScreenSound/ScreenSound/Menus/Menu.cs
@@ -6,11 +6,15 @@ internal class Menu
 {
     public void ExibirTituloOpcao(string titulo)
     {
-
+        int quantidadeDeLetras = titulo.Length;
+        string asteriscos = string.Empty.PadLeft(quantidadeDeLetras, '*');
+        Console.WriteLine(asteriscos);
+        Console.WriteLine(titulo);
+        Console.WriteLine(asteriscos + "\n");
     }
 
     public virtual void Executar(Dictionary<string, Banda> bandasRegistradas)
     {
-
+        Console.Clear();
     }
 }
