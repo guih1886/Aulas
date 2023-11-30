f4c61853510cdacd122204e31bca9b6e50b9160b
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2023-11-30 08:38:00 -0300

    Update MenuMostrarBandas.cs

--- C#/ScreenSound/ScreenSound/Menus/MenuMostrarBandas.cs
+++ C#/ScreenSound/ScreenSound/Menus/MenuMostrarBandas.cs
@@ -6,5 +6,15 @@ internal class MenuMostrarBandas : Menu
     public override void Executar(Dictionary<string, Banda> bandasRegistradas)
     {
         base.Executar(bandasRegistradas);
+        ExibirTituloOpcao("Exibindo todas as bandas registradas na nossa aplicação");
+
+        foreach (string banda in bandasRegistradas.Keys)
+        {
+            Console.WriteLine($"Banda: {banda}");
+        }
+
+        Console.WriteLine("\nDigite uma tecla para voltar ao menu principal");
+        Console.ReadKey();
+        Console.Clear();
     }
 }
