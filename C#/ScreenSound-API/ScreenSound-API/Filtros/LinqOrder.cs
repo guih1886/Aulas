1a114063ee481d0fdeb4ea820fc16806b046e374
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2023-11-30 20:55:28 -0300

    Create LinqOrder.cs

---
+++ C#/ScreenSound-API/ScreenSound-API/Filtros/LinqOrder.cs
@@ -0,0 +1,15 @@
+﻿using ScreenSound_API.Modelos;
+
+namespace ScreenSound_API.Filtros;
+
+internal class LinqOrder
+{
+    public static void OrdenarListaDeArtista(List<Musica> musicas)
+    {
+        var artistasOrdenados = musicas.OrderBy(musicas => musicas.Artista).Select(musica => musica.Artista).Distinct().ToList();
+        foreach (var artista in artistasOrdenados)
+        {
+            Console.WriteLine($"Artistas - {artista}");
+        }
+    }
+}
