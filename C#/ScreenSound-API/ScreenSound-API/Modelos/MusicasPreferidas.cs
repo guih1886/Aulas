4ed07b5c70a6a2016b6636aeeb18af354506b554
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2023-11-30 20:55:40 -0300

    Create MusicasPreferidas.cs

---
+++ C#/ScreenSound-API/ScreenSound-API/Modelos/MusicasPreferidas.cs
@@ -0,0 +1,40 @@
+﻿using System.Text.Json;
+
+namespace ScreenSound_API.Modelos;
+
+internal class MusicasPreferidas
+{
+    public string Nome { get; set; }
+    public List<Musica> ListaDeMusicasFavoritas { get; }
+
+    public MusicasPreferidas(string nome)
+    {
+        Nome = nome;
+        ListaDeMusicasFavoritas = new List<Musica>();
+    }
+
+    public void AdicionarMusicasFavoritas(Musica musica)
+    {
+        ListaDeMusicasFavoritas.Add(musica);
+    }
+
+    public void ExibirMusicasFavoritas()
+    {
+        Console.WriteLine($"Exibindo músicas favoritas de {Nome}");
+        foreach (var musica in ListaDeMusicasFavoritas)
+        {
+            Console.WriteLine($"- {musica.Nome} de {musica.Artista}");
+        }
+    }
+
+    public void GerarArquivoJson()
+    {
+        string json = JsonSerializer.Serialize(new
+        {
+            nome = Nome,
+            musicas = ListaDeMusicasFavoritas,
+        });
+        string nomeDoArquivo = $"musicas-favoritas-{Nome}.json";
+        File.WriteAllText(nomeDoArquivo, json);
+    }
+}
