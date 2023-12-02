12d952b113f4f1bd29c7422e15b032e88f2df140
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2023-12-01 23:45:01 -0300

    Create ListaDeContaCorrente.cs

---
+++ C#/ByteBank/ByteBank/Util/ListaDeContaCorrente.cs
@@ -0,0 +1,29 @@
+﻿using bytebank.Modelos.Conta;
+
+namespace ByteBank.Util;
+
+public class ListaDeContaCorrente
+{
+    private ContaCorrente[] _items = new ContaCorrente[] { };
+
+    public ListaDeContaCorrente(int tamanhoInicial = 5)
+    {
+        _items = new ContaCorrente[tamanhoInicial];
+    }
+
+    public void Adicionar(ContaCorrente conta)
+    {
+        _items.Append(conta);
+    }
+    
+    public void Remover(ContaCorrente conta)
+    {
+        //_items.Select(conta => conta == conta);
+
+    }
+
+    public int ContarContas()
+    {
+        return _items.Length;
+    }
+}
