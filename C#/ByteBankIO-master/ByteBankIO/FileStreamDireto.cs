9805ec29d6562066b2bdd9b373d6bc5da8b29095
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2023-12-06 20:43:24 -0300

    c# class project creation
    c# class project creation

---
+++ C#/ByteBankIO-master/ByteBankIO/FileStreamDireto.cs
@@ -0,0 +1,39 @@
+﻿using System.Text;
+
+namespace ByteBankIO;
+
+partial class Program
+{
+    static void FileStreamDireto()
+    {
+        var enderecoDoArquivo = "contas.txt";
+
+        using (var fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open))
+        {
+            var numeroDeBytesLidos = -1;
+
+            var buffer = new byte[1024]; //1KB
+
+            while (numeroDeBytesLidos != 0)
+            {
+                numeroDeBytesLidos = fluxoDoArquivo.Read(buffer, 0, buffer.Length);
+                EscreverBuffer(buffer, numeroDeBytesLidos);
+            }
+            fluxoDoArquivo.Close();
+            Console.ReadKey();
+        }
+    }
+
+    static void EscreverBuffer(byte[] buffer, int bytesLidos)
+    {
+        var utf8 = new UTF8Encoding();
+        var texto = utf8.GetString(buffer, 0, bytesLidos);
+        Console.Write(texto);
+        //foreach (var item in buffer)
+        //{
+        //    Console.Write(item);
+        //    Console.Write(" ");
+        //}
+    }
+}
+
---
+++ C#/ByteBankIO-master/ByteBankIO/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ByteBankIO;
+
+partial class Program
+{
+    static void Main(string[] args)
+    {
+        var enderecoDoArquivo = "contas.txt";
+
+        using (var fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open))
+        {
+            var leitor = new StreamReader(fluxoDoArquivo);
+
+            //var linha = leitor.ReadLine();
+            var texto = leitor.ReadToEnd();
+            Console.WriteLine(texto);
+        }
+        Console.ReadKey();
+    }
+}
