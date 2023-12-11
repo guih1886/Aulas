ab737fb4841b36f56d5b7e1755ab741660aac0dd
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2023-12-11 19:38:54 -0300

    asp.net project filmesapi creation
    asp.net project filmesapi creation

---
+++ C#/FilmesAPI/FilmesAPI/Controllers/FilmeController.cs
@@ -0,0 +1,25 @@
+﻿using FilmesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class FilmeController : ControllerBase
+{
+
+    [HttpPost]
+    public void AdicionarFilme([FromBody] Filme filme)
+    {
+        Filme.Filmes.Add(filme);
+        Console.WriteLine(filme.Titulo);
+        Console.WriteLine(filme.Genero);
+        Console.WriteLine(filme.Duracao);
+    }
+
+    [HttpGet]
+    public List<Filme> ListarFilmes()
+    {
+        return Filme.Filmes.ToList();
+    }
+}
---
+++ C#/FilmesAPI/FilmesAPI/Models/Filme.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Models
+{
+    public class Filme
+    {
+        // Lista de filmes
+        public static List<Filme> Filmes = new List<Filme>();
+
+        [Required(ErrorMessage = "O título do filme é obrigatório.")]
+        public string Titulo { get; set; }
+
+        [Required(ErrorMessage = "O genero do filme é obrigatório.")]
+        public string Genero { get; set; }
+
+        [Required(ErrorMessage = "A duração do filme é obrigatório.")]
+        [Range(70, 600, ErrorMessage = "A duração deve ser entre 70 e 600 minutos.")]
+        public int Duracao { get; set; }
+    }
+}
