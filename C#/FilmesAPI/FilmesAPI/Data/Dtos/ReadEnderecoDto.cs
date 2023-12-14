1bb1373c4a2065ccab7ce55955ba6c2d6d9f4089
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2023-12-14 19:51:30 -0300

    .net class create dtos
    .net class create dtos

---
+++ C#/FilmesAPI/FilmesAPI/Data/Dtos/ReadEnderecoDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Data.Dtos;
+
+public class ReadEnderecoDto
+{
+    public int Id { get; set; }
+    [Required(ErrorMessage = "O logradouro do endereço é obrigatório")]
+    public string Logradouro { get; set; }
+    [Required]
+    public int Numero { get; set; }
+}
---
+++ C#/FilmesAPI/FilmesAPI/Data/Dtos/UpdateEnderecoDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Data.Dtos;
+
+public class UpdateEnderecoDto
+{
+    [Required(ErrorMessage = "O logradouro do endereço é obrigatório")]
+    public string Logradouro { get; set; }
+    [Required]
+    public int Numero { get; set; }
+}
