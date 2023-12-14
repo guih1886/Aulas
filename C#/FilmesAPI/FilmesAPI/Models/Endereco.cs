3a15e9236e8741d4f21e6dbb8c6bb2760051057f
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2023-12-14 19:51:45 -0300

    Create Endereco.cs

---
+++ C#/FilmesAPI/FilmesAPI/Models/Endereco.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Models;
+
+public class Endereco
+{
+    [Key]
+    [Required]
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "O logradouro do endereço é obrigatório")]
+    public string Logradouro { get; set; }
+    [Required]
+    public int Numero { get; set; }
+    public virtual Cinema Cinema { get; set; }
+}
