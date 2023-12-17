e5e04874cdb18793ccef8a273085b2468c9be8bb
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2023-12-16 22:43:15 -0300

    .net class models update
    .net class models update

--- C#/FilmesAPI/FilmesAPI/Models/Filme.cs
+++ C#/FilmesAPI/FilmesAPI/Models/Filme.cs
@@ -5,15 +5,7 @@ namespace FilmesAPI.Models
     public class Filme
     {
         // Lista de filmes
-        public static List<Filme> Filmes = new List<Filme>
-       {
-            new Filme { Id = 1, Titulo = "Filme 1", Genero = Genero.DRAMA, Duracao = 120 },
-            new Filme { Id = 2, Titulo = "Filme 2", Genero = Genero.TERROR, Duracao = 95 },
-            new Filme { Id = 3, Titulo = "Filme 3", Genero = Genero.SUSPENSE, Duracao = 95 },
-            new Filme { Id = 4, Titulo = "Filme 4", Genero = Genero.DOCUMENTARIO, Duracao = 95 },
-            new Filme { Id = 5, Titulo = "Filme 5", Genero = Genero.DOCUMENTARIO, Duracao = 95 },
-            new Filme { Id = 6, Titulo = "Filme 6", Genero = Genero.FICCAO, Duracao = 95 },
-        };
+        public static List<Filme> Filmes = new List<Filme>();
         [Key]
         public int Id { get; set; }
 
@@ -21,7 +13,7 @@ namespace FilmesAPI.Models
         public string Titulo { get; set; }
 
         [Required(ErrorMessage = "O genero do filme é obrigatório.")]
-        public Genero Genero { get; set; }
+        public string Genero { get; set; }
 
         [Required(ErrorMessage = "A duração do filme é obrigatório.")]
         [Range(70, 600, ErrorMessage = "A duração deve ser entre 70 e 600 minutos.")]
--- C#/FilmesAPI/FilmesAPI/Models/Sessao.cs
+++ C#/FilmesAPI/FilmesAPI/Models/Sessao.cs
@@ -4,10 +4,9 @@ namespace FilmesAPI.Models;
 
 public class Sessao
 {
-    [Key]
-    [Required]
-    public int Id { get; set; }
-    [Required]
-    public int FilmeId { get; set; }
+    public int? FilmeId { get; set; }
     public virtual Filme Filme { get; set; }
+    public int? CinemaId { get; set; }
+
+    public virtual Cinema Cinema { get; set; }
 }
