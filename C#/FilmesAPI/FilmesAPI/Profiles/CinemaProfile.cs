be7c9daf29ae9d5501b332c9444a4a3fe5fe4728
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2023-12-16 22:43:03 -0300

    .net class profiles update
    .net class profiles update

--- C#/FilmesAPI/FilmesAPI/Profiles/CinemaProfile.cs
+++ C#/FilmesAPI/FilmesAPI/Profiles/CinemaProfile.cs
@@ -10,6 +10,8 @@ public class CinemaProfile : Profile
     {
         CreateMap<CreateCinemaDto, Cinema>();
         CreateMap<UpdateCinemaDto, Cinema>();
-        CreateMap<Cinema, ReadCinemaDto>().ForMember(cinemaDto => cinemaDto.Endereco, opt => opt.MapFrom(cinema => cinema.Endereco));
+        CreateMap<Cinema, ReadCinemaDto>()
+            .ForMember(cinemaDto => cinemaDto.Endereco, opt => opt.MapFrom(cinema => cinema.Endereco))
+            .ForMember(cinemaDto => cinemaDto.Sessoes, opt => opt.MapFrom(cinema => cinema.Sessoes));
     }
 }
