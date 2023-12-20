0dbd214efed9393f108b6b258d4a275ac77a2b25
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2023-12-20 20:06:45 -0300

    asp.net authorization create
    asp.net authorization create

---
+++ C#/UsuariosAPI/UsuariosAPI/Authorization/IdadeAuthorization.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System.Security.Claims;
+
+namespace UsuariosAPI.Authorization;
+
+
+public class IdadeAuthorization : AuthorizationHandler<IdadeMinima>
+{
+    protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, IdadeMinima requirement)
+    {
+        var dataNascimentoClaim = context.User.FindFirst(claim => claim.Type == ClaimTypes.DateOfBirth);
+
+        if (dataNascimentoClaim is null)
+        {
+            return Task.CompletedTask;
+        }
+
+        var dataNascimento = Convert.ToDateTime(dataNascimentoClaim.Value);
+
+        var idade = DateTime.Now.Year - dataNascimento.Year;
+
+        if (dataNascimento > DateTime.Today.AddYears(-idade)) idade--;
+
+        if (idade >= requirement.Idade) context.Succeed(requirement);
+
+        return Task.CompletedTask;
+    }
+}
