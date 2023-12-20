425285194b2ced85c30c09e5a7df75094c4e194d
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2023-12-20 20:07:48 -0300

    Update Program.cs

--- C#/UsuariosAPI/UsuariosAPI/Program.cs
+++ C#/UsuariosAPI/UsuariosAPI/Program.cs
@@ -1,5 +1,10 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using UsuariosAPI.Authorization;
 using UsuariosAPI.Data;
 using UsuariosAPI.Models;
 using UsuariosAPI.Services;
@@ -19,13 +24,28 @@ builder.Services.AddIdentity<Usuario, IdentityRole>()
     .AddDefaultTokenProviders();
 
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
-builder.Services.AddScoped<CadastroService>();
+builder.Services.AddScoped<UsuarioService>();
+builder.Services.AddSingleton<IAuthorizationHandler, IdadeAuthorization>();
+builder.Services.AddScoped<TokenService>();
 
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddAuthorization(ops =>
+    ops.AddPolicy("idadeMinima", policy => policy.AddRequirements(new IdadeMinima(18)))
+);
+builder.Services.AddAuthentication(ops =>
+    ops.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme
+).AddJwtBearer(ops => ops.TokenValidationParameters = new TokenValidationParameters
+{
+    ValidateIssuerSigningKey = true,
+    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("WE8FA2W23FA969W1S")),
+    ValidateAudience = false,
+    ValidateIssuer = false,
+    ClockSkew = TimeSpan.Zero
+});
 
 var app = builder.Build();
 
@@ -37,7 +57,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
