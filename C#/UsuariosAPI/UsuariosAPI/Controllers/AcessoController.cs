7ffab3f0996e4c71c2cd18c9e446274e5bc93c7e
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2023-12-20 20:07:10 -0300

    asp.net acesso controller creation
    asp.net acesso controller creation

---
+++ C#/UsuariosAPI/UsuariosAPI/Controllers/AcessoController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UsuariosAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class AcessoController : ControllerBase
+{
+    [HttpGet]
+    [Authorize(Policy = "idadeMinima")]
+    public IActionResult Get()
+    {
+        return Ok("Acesso permitido.");
+    }
+}
