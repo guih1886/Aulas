883c499c2bf92deb85f6673ce385d17abd812975
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2023-12-20 20:07:37 -0300

    asp.net services creation
    asp.net services creation

---
+++ C#/UsuariosAPI/UsuariosAPI/Services/TokenService.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using UsuariosAPI.Models;
+
+namespace UsuariosAPI.Services;
+
+public class TokenService
+{
+    public string GenerateToken(Usuario usuario)
+    {
+        //reinvidicações do usuário
+        Claim[] claims = new Claim[]
+        {
+            new Claim("username", usuario.UserName),
+            new Claim("id", usuario.Id),
+            new Claim(ClaimTypes.DateOfBirth, usuario.DataNascimento.ToString()),
+        };
+
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("WE8FA2W23FA969W1S"));
+        var signingCredentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+        var token = new JwtSecurityToken(
+            expires: DateTime.Now.AddHours(1),
+            claims: claims,
+            signingCredentials: signingCredentials
+            );
+
+        return new JwtSecurityTokenHandler().WriteToken(token);
+    }
+}
