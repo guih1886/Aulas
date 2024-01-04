662f27d82c15e322166cdce07e1781035a169c05
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2024-01-03 21:01:07 -0300

    .net tests class creation
    .net tests class creation

---
+++ C#/Alura.ByteBank/Alura.BytetBank.Infra.Testes/ByteBankContextoTestes.cs
@@ -0,0 +1,13 @@
+﻿using Alura.ByteBank.Dados.Contexto;
+
+namespace Alura.BytetBank.Infraestrutura.Testes;
+
+public class ByteBankContextoTestes
+{
+    public void TestaConexaoContextoComBDMysql()
+    {
+        //Arrange
+        var contexto = new ByteBankContexto();
+        bool conectado;
+    }
+}
