664b01f8e75ddaea891ee512d10936a68c0754a4
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2024-01-05 15:48:45 -0300

    Create IPixRepositorio.cs

---
+++ C#/Alura.ByteBank/Alura.BytetBank.Infra.Testes/Servico/IPixRepositorio.cs
@@ -0,0 +1,8 @@
+﻿using Alura.ByteBank.Infraestrutura.Testes.Servico.DTO;
+
+namespace Alura.ByteBank.Infraestrutura.Testes.Servico;
+
+public interface IPixRepositorio
+{
+    public PixDTO consultaPix(Guid pix);
+}
