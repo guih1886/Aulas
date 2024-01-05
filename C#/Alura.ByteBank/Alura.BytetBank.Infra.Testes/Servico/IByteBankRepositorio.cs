337a81d63f52ca89fee9c1623e277d846cf68f24
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2024-01-05 15:48:43 -0300

    Create IByteBankRepositorio.cs

---
+++ C#/Alura.ByteBank/Alura.BytetBank.Infra.Testes/Servico/IByteBankRepositorio.cs
@@ -0,0 +1,11 @@
+﻿using Alura.ByteBank.Dominio.Entidades;
+
+namespace Alura.ByteBank.Infraestrutura.Testes
+{
+    public interface IByteBankRepositorio
+    {
+        public List<Cliente> BuscarClientes();
+        public List<Agencia> BuscarAgencias();
+        public List<ContaCorrente> BuscarContasCorrentes();
+    }
+}
