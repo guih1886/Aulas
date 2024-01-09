5a107e6cd5834ccd93ae85180d6eb7ffd6816c16
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2024-01-08 22:42:29 -0300

    Create LoginPO.cs

---
+++ C#/Alura.ByteBank/Alura.ByteBank.Apresentacao.Test/PageObjects/LoginPO.cs
@@ -0,0 +1,37 @@
+﻿using OpenQA.Selenium;
+using System;
+
+namespace Alura.ByteBank.Apresentacao.Testes.PageObjects
+{
+    public class LoginPO
+    {
+        private IWebDriver driver;
+        private By campoEmail;
+        private By campoSenha;
+        private By btnLogar;
+
+        public LoginPO(IWebDriver driver)
+        {
+            this.driver = driver;
+            campoEmail = By.Id("Email");
+            campoSenha = By.Id("Senha");
+            btnLogar = By.Id("btn-logar");
+        }
+
+        public void Navegar(string url)
+        {
+            driver.Navigate().GoToUrl(url);
+        }
+
+        public void PreencherCamposELogar(string email, string senha)
+        {
+            driver.FindElement(campoEmail).SendKeys(email);
+            driver.FindElement(campoSenha).SendKeys(senha);
+        }
+
+        public void btnClick()
+        {
+            driver.FindElement(btnLogar).Click();
+        }
+    }
+}
