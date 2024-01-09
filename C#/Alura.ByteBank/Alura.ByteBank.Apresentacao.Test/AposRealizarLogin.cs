b6f795dfdadd69c62eba02b469cd29b7bf5fe2f5
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2024-01-08 22:42:33 -0300

    Update AposRealizarLogin.cs

--- C#/Alura.ByteBank/Alura.ByteBank.Apresentacao.Test/AposRealizarLogin.cs
+++ C#/Alura.ByteBank/Alura.ByteBank.Apresentacao.Test/AposRealizarLogin.cs
@@ -3,6 +3,7 @@ using OpenQA.Selenium;
 using Xunit.Abstractions;
 using Xunit;
 using System;
+using Alura.ByteBank.Apresentacao.Testes.PageObjects;
 
 namespace Alura.ByteBank.Apresentacao.Testes
 {
@@ -11,23 +12,22 @@ namespace Alura.ByteBank.Apresentacao.Testes
         public IWebDriver driver { get; private set; }
         private ITestOutputHelper output;
         private string url = "https://localhost:5001/";
+        private LoginPO loginPO;
+        private ClientePO clientePO;
         public AposRealizarLogin(ITestOutputHelper _output)
         {
-            this.output = _output;
             driver = new ChromeDriver();
+            this.output = _output;
+            this.loginPO = new LoginPO(driver);
+            this.clientePO = new ClientePO(driver);
             LogarNoSistema("andre@email.com", "senha01");
         }
 
         public void LogarNoSistema(string email, string senha)
         {
-            driver.Navigate().GoToUrl(url);
-            driver.Manage().Window.Size = new System.Drawing.Size(1294, 1032);
-            driver.FindElement(By.LinkText("Login")).Click();
-            driver.FindElement(By.Id("Email")).Click();
-            driver.FindElement(By.Id("Email")).SendKeys(email);
-            driver.FindElement(By.Id("Senha")).Click();
-            driver.FindElement(By.Id("Senha")).SendKeys(senha);
-            driver.FindElement(By.Id("btn-logar")).Click();
+            loginPO.Navegar(url + "UsuarioApps/Login");
+            loginPO.PreencherCamposELogar(email, senha);
+            loginPO.btnClick();
         }
 
         [Fact]
@@ -45,28 +45,16 @@ namespace Alura.ByteBank.Apresentacao.Testes
         public void CadastraCliente()
         {
             //Arrange
-            var guid = Guid.NewGuid().ToString();
+            var guid = Guid.NewGuid();
 
-            //Act
-            driver.FindElement(By.LinkText("Cliente")).Click();
-            driver.FindElement(By.LinkText("Adicionar Cliente")).Click();
-
-            driver.FindElement(By.Name("Identificador")).Click();
-            driver.FindElement(By.Name("Identificador")).SendKeys(guid);
-
-            driver.FindElement(By.Name("CPF")).Click();
-            driver.FindElement(By.Name("CPF")).SendKeys("601.811.150-00");
-
-            driver.FindElement(By.Name("Nome")).Click();
-            driver.FindElement(By.Name("Nome")).SendKeys("Adelberto Gaudencio");
-
-            driver.FindElement(By.Name("Profissao")).Click();
-            driver.FindElement(By.Name("Profissao")).SendKeys("Lavrador");
-
-            driver.FindElement(By.CssSelector(".btn-primary")).Click();
+            //Act          
+            clientePO.Navegar(url + "Clientes/Index");
+            clientePO.ClicarAdicionarCliente();
+            clientePO.PreencheFormularioAdicionarCliente(guid, "601.811.150-00", "Adelberto Gaudencio", "Lavrador");
+            clientePO.btnCliente();
 
             //Assert
-            Assert.Contains(guid, driver.PageSource);
+            Assert.Contains(guid.ToString(), driver.PageSource);
         }
 
         public void Dispose()
