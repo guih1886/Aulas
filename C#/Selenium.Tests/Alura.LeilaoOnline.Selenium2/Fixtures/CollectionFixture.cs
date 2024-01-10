c248c19df5668002fa47325232259cb282919055
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2024-01-09 23:32:00 -0300

    fixture creation
    fixture creation

---
+++ C#/Selenium.Tests/Alura.LeilaoOnline.Selenium2/Fixtures/CollectionFixture.cs
@@ -0,0 +1,11 @@
+﻿using Alura.LeilaoOnline.Selenium2.Fixture;
+using Xunit;
+
+namespace Alura.LeilaoOnline.Selenium2.Fixtures
+{
+    [CollectionDefinition("Chrome Driver")]
+    public class CollectionFixture : ICollectionFixture<TestFixture>
+    {
+
+    }
+}
---
+++ C#/Selenium.Tests/Alura.LeilaoOnline.Selenium2/Fixtures/TestFixture.cs
@@ -0,0 +1,20 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Alura.LeilaoOnline.Selenium2.Fixture
+{
+    public class TestFixture : IDisposable
+    {
+        public IWebDriver Driver { get; private set; }
+
+        public TestFixture()
+        {
+            Driver = new ChromeDriver();
+        }
+
+        public void Dispose()
+        {
+            Driver.Quit();
+        }
+    }
+}
