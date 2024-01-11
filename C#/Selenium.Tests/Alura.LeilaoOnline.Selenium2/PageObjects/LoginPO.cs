0792b4e3df52352f67e743a5557d6d7734aed7f4
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2024-01-11 00:22:25 -0300

    Create LoginPO.cs

---
+++ C#/Selenium.Tests/Alura.LeilaoOnline.Selenium2/PageObjects/LoginPO.cs
@@ -0,0 +1,38 @@
+﻿
+using OpenQA.Selenium;
+
+namespace Alura.LeilaoOnline.Selenium2.PageObjects
+{
+    internal class LoginPO
+    {
+        private IWebDriver driver;
+
+        private By inputLogin;
+        private By inputSenha;
+        private By botaoLogin;
+
+        public LoginPO(IWebDriver driver)
+        {
+            this.driver = driver;
+            inputLogin = By.Id("Login");
+            inputSenha = By.Id("Password");
+            botaoLogin = By.Id("btnLogin");
+        }
+
+        public void VisitarUrlBase()
+        {
+            driver.Navigate().GoToUrl("http://localhost:5000/Autenticacao/Login");
+        }
+
+        public void PreencheFormularioLogin(string login, string senha)
+        {
+            driver.FindElement(inputLogin).SendKeys(login);
+            driver.FindElement(inputSenha).SendKeys(senha);
+        }
+
+        public void SubmeterFormularioLogin()
+        {
+            driver.FindElement(botaoLogin).Submit();
+        }
+    }
+}
