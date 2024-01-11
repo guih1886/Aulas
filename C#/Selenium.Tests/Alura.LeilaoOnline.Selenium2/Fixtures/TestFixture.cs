d7a40cf5a2b1a25ec24a14a8702a4dc3ddf89838
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2024-01-10 22:30:27 -0300

    Update TestFixture.cs

--- C#/Selenium.Tests/Alura.LeilaoOnline.Selenium2/Fixtures/TestFixture.cs
+++ C#/Selenium.Tests/Alura.LeilaoOnline.Selenium2/Fixtures/TestFixture.cs
@@ -1,11 +1,14 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using Xunit.Abstractions;
 
 namespace Alura.LeilaoOnline.Selenium2.Fixture
 {
     public class TestFixture : IDisposable
     {
         public IWebDriver Driver { get; private set; }
+
+        public string urlBase = "http://localhost:5000/";
 
         public TestFixture()
         {
