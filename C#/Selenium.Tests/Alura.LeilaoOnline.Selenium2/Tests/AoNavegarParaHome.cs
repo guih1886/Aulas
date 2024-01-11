0c50e6758fe0ea5fda3bbf50ff55ca98a814d386
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2024-01-10 22:30:32 -0300

    Update AoNavegarParaHome.cs

--- C#/Selenium.Tests/Alura.LeilaoOnline.Selenium2/Tests/AoNavegarParaHome.cs
+++ C#/Selenium.Tests/Alura.LeilaoOnline.Selenium2/Tests/AoNavegarParaHome.cs
@@ -1,6 +1,8 @@
 using Alura.LeilaoOnline.Selenium2.Fixture;
 using OpenQA.Selenium;
+using System.Collections.ObjectModel;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace Alura.LeilaoOnline.Selenium2.Tests
 {
@@ -8,11 +10,13 @@ namespace Alura.LeilaoOnline.Selenium2.Tests
     public class AoNavegarParaHome
     {
         private IWebDriver driver;
+        private ITestOutputHelper output;
 
         //Setup
-        public AoNavegarParaHome(TestFixture fixture)
+        public AoNavegarParaHome(TestFixture fixture, ITestOutputHelper _output)
         {
             driver = fixture.Driver;
+            output = _output;
         }
 
         [Fact]
@@ -32,6 +36,20 @@ namespace Alura.LeilaoOnline.Selenium2.Tests
             driver.Navigate().GoToUrl("http://localhost:5000/");
             //Assert
             Assert.Contains("Próximos Leilões", driver.PageSource);
+        }
+
+        [Fact]
+        public void DadoNavegadorAbertoNaoDeveMostrarMensagensDeErro()
+        {
+            //Arrange
+            driver.Navigate().GoToUrl("http://localhost:5000/");
+            var erros = driver.FindElements(By.CssSelector("form span"));
+            //Act
+            //Assert
+            foreach (var erro in erros)
+            {
+                Assert.True(string.IsNullOrEmpty(erro.Text));
+            }
         }
     }
 }
