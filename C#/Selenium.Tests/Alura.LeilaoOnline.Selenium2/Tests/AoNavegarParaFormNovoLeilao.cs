94a0b92b50d0784416b701a9fe44f025b1677a19
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2024-01-11 19:50:36 -0300

    Create AoNavegarParaFormNovoLeilao.cs

---
+++ C#/Selenium.Tests/Alura.LeilaoOnline.Selenium2/Tests/AoNavegarParaFormNovoLeilao.cs
@@ -0,0 +1,37 @@
+﻿using Alura.LeilaoOnline.Selenium2.Fixture;
+using Alura.LeilaoOnline.Selenium2.PageObjects;
+using OpenQA.Selenium;
+using Xunit;
+
+namespace Alura.LeilaoOnline.Selenium2.Tests
+{
+    [Collection("Chrome Driver")]
+    public class AoNavegarParaFormNovoLeilao
+    {
+        private IWebDriver driver;
+        private LoginPO loginPO;
+        private NovoLeilaoPO novoLeilaoPO;
+
+        public AoNavegarParaFormNovoLeilao(TestFixture fixture)
+        {
+            this.driver = fixture.Driver;
+            loginPO = new LoginPO(this.driver);
+            novoLeilaoPO = new NovoLeilaoPO(this.driver);
+        }
+
+        [Fact]
+        public void DadoLoginAdmDeveMostrarTresCategorias()
+        {
+            //Assert
+            loginPO.VisitarUrlLogin();
+            loginPO.PreencheFormularioLogin("fulano@example.org", "123");
+            loginPO.SubmeterFormularioLogin();
+
+            //Act
+            novoLeilaoPO.Visitar();
+
+            //Assert
+            Assert.Equal(3, novoLeilaoPO.Categorias.Count());
+        }
+    }
+}
