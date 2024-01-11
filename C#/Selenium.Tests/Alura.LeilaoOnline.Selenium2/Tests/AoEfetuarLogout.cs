3fdfb773aef064cbe890204ef8545894d279b548
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2024-01-11 19:50:27 -0300

    Create AoEfetuarLogout.cs

---
+++ C#/Selenium.Tests/Alura.LeilaoOnline.Selenium2/Tests/AoEfetuarLogout.cs
@@ -0,0 +1,37 @@
+﻿using Alura.LeilaoOnline.Selenium2.Fixture;
+using Alura.LeilaoOnline.Selenium2.PageObjects;
+using OpenQA.Selenium;
+using Xunit;
+
+namespace Alura.LeilaoOnline.Selenium2.Tests
+{
+    [Collection("Chrome Driver")]
+    public class AoEfetuarLogout
+    {
+        private IWebDriver driver;
+        private LoginPO loginPO;
+        private DashboardInteressadaPO dashboardPO;
+
+        public AoEfetuarLogout(TestFixture fixture)
+        {
+            driver = fixture.Driver;
+            loginPO = new LoginPO(driver);
+            dashboardPO = new DashboardInteressadaPO(driver);
+        }
+
+        [Fact]
+        public void DadoLoginValidoDeveIrParaHome()
+        {
+            //Arrange
+            loginPO.VisitarUrlLogin();
+            loginPO.PreencheFormularioLogin("fulano@example.org", "123");
+            loginPO.SubmeterFormularioLogin();
+
+            //Act
+            dashboardPO.EfetuarLogout();
+
+            //Assert
+            Assert.Contains("Próximos Leilões", driver.PageSource);
+        }
+    }
+}
