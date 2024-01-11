a3bb52a55dfd8781c3bdf5efed26a9890789283a
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2024-01-11 00:22:28 -0300

    Create AoEfetuarLogin.cs

---
+++ C#/Selenium.Tests/Alura.LeilaoOnline.Selenium2/Tests/AoEfetuarLogin.cs
@@ -0,0 +1,44 @@
+﻿using Alura.LeilaoOnline.Selenium2.Fixture;
+using Alura.LeilaoOnline.Selenium2.PageObjects;
+using OpenQA.Selenium;
+using Xunit;
+
+namespace Alura.LeilaoOnline.Selenium2.Tests
+{
+    [Collection("Chrome Driver")]
+    public class AoEfetuarLogin
+    {
+        private IWebDriver driver;
+        private LoginPO loginPO;
+
+        public AoEfetuarLogin(TestFixture fixture)
+        {
+            driver = fixture.Driver;
+            loginPO = new LoginPO(driver);
+        }
+
+        [Fact]
+        public void ValidaCredenciaisLoginCorrretas()
+        {
+            //Arrange
+            loginPO.VisitarUrlBase();
+            //Act
+            loginPO.PreencheFormularioLogin("fulano@example.org", "123");
+            loginPO.SubmeterFormularioLogin();
+            //Assert
+            Assert.Contains("Dashboard", driver.PageSource);
+        }
+
+        [Fact]
+        public void ValidaCredenciaisLoginIncorrretas()
+        {
+            //Arrange
+            loginPO.VisitarUrlBase();
+            //Act
+            loginPO.PreencheFormularioLogin("fulano@emaple.org.com", "");
+            loginPO.SubmeterFormularioLogin();
+            //Assert
+            Assert.Contains("Login", driver.PageSource);
+        }
+    }
+}
