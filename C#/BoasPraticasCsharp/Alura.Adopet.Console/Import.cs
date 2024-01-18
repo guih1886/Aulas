3937296e325811f4a834327763aafdc733b46b89
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2024-01-17 21:24:13 -0300

    Create Import.cs

---
+++ C#/BoasPraticasCsharp/Alura.Adopet.Console/Import.cs
@@ -0,0 +1,40 @@
+﻿using System.Net.Http.Headers;
+using System.Net.Http.Json;
+
+namespace Alura.Adopet.Console
+{
+    public class Import
+    {
+        HttpClient client;
+
+        public Import()
+        {
+            client = new Util().ConfiguraHttpClient("http://localhost:5057");
+        }
+        public async Task ImportacaoArquivoPetAsync(string caminhoDoArquivoDeImportacao)
+        {
+            LeitorDeArquivo leitor = new LeitorDeArquivo();
+            List<Pet> listaDePet = leitor.RealizaLeitura(caminhoDoArquivoDeImportacao);
+            foreach (var pet in listaDePet)
+            {
+                try
+                {
+                    var resposta = await CreatePetAsync(pet);
+                }
+                catch (Exception ex)
+                {
+                    System.Console.WriteLine(ex.Message);
+                }
+            }
+            System.Console.WriteLine("Importação concluída!");
+        }
+        Task<HttpResponseMessage> CreatePetAsync(Pet pet)
+        {
+            HttpResponseMessage? response = null;
+            using (response = new HttpResponseMessage())
+            {
+                return client.PostAsJsonAsync("pet/add", pet);
+            }
+        }
+    }
+}
