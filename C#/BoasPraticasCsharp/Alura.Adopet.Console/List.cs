13e6681bf0aa9b6560c1cfe671880b6cba651a9e
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2024-01-17 21:24:22 -0300

    Create List.cs

---
+++ C#/BoasPraticasCsharp/Alura.Adopet.Console/List.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Net.Http.Json;
+
+namespace Alura.Adopet.Console
+{
+    public class List
+    {
+        HttpClient client;
+        public List()
+        {
+            client = new Util().ConfiguraHttpClient("http://localhost:5057");
+        }
+        public async Task ListarPetsAsync()
+        {
+            var pets = await ListPetsAsync();
+            foreach (var pet in pets)
+            {
+                System.Console.WriteLine(pet);
+            }
+        }
+
+        async Task<IEnumerable<Pet>?> ListPetsAsync()
+        {
+            HttpResponseMessage response = await client.GetAsync("pet/list");
+            return await response.Content.ReadFromJsonAsync<IEnumerable<Pet>>();
+        }
+    }
+}
