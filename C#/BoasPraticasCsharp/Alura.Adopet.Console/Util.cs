038b1b653ade6c355891735f47b4a04f1bfe04da
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2024-01-17 21:24:27 -0300

    Create Util.cs

---
+++ C#/BoasPraticasCsharp/Alura.Adopet.Console/Util.cs
@@ -0,0 +1,17 @@
+﻿using System.Net.Http.Headers;
+
+namespace Alura.Adopet.Console
+{
+    public class Util
+    {
+        public HttpClient ConfiguraHttpClient(string url)
+        {
+            HttpClient _client = new HttpClient();
+            _client.DefaultRequestHeaders.Accept.Clear();
+            _client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+            _client.BaseAddress = new Uri(url);
+            return _client;
+        }
+    }
+}
