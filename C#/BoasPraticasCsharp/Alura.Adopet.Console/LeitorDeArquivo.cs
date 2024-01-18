5765b2013fd2742f08b4b5536eea367f5d325ceb
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2024-01-17 21:24:16 -0300

    Create LeitorDeArquivo.cs

---
+++ C#/BoasPraticasCsharp/Alura.Adopet.Console/LeitorDeArquivo.cs
@@ -0,0 +1,27 @@
+﻿namespace Alura.Adopet.Console
+{
+    public class LeitorDeArquivo
+    {
+        public List<Pet> RealizaLeitura(string caminhoDoArquivoASerLido)
+        {
+            List<Pet> listaDePet = new List<Pet>();
+            using (StreamReader sr = new StreamReader(caminhoDoArquivoASerLido))
+            {
+                while (!sr.EndOfStream)
+                {
+                    // separa linha usando ponto e vírgula
+                    string[] propriedades = sr.ReadLine().Split(';');
+                    // cria objeto Pet a partir da separação
+                    Pet pet = new Pet(Guid.Parse(propriedades[0]),
+                      propriedades[1],
+                      TipoPet.Cachorro
+                     );
+
+                    System.Console.WriteLine(pet);
+                    listaDePet.Add(pet);
+                }
+            }
+            return listaDePet;
+        }
+    }
+}
