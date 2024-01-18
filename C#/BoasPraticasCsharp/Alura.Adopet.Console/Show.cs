2a5a86e33e763aa6360a94f7352005b4aa1b96c6
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2024-01-17 21:24:25 -0300

    Create Show.cs

---
+++ C#/BoasPraticasCsharp/Alura.Adopet.Console/Show.cs
@@ -0,0 +1,15 @@
+﻿namespace Alura.Adopet.Console
+{
+    public class Show
+    {
+        public void MostrarPetsASerImportado(string caminhoDoArquivoASerExibido)
+        {
+            LeitorDeArquivo leitor = new LeitorDeArquivo();
+            var listaDePets = leitor.RealizaLeitura(caminhoDoArquivoASerExibido);
+            foreach (var item in listaDePets)
+            {
+                System.Console.WriteLine(item);
+            }
+        }
+    }
+}
