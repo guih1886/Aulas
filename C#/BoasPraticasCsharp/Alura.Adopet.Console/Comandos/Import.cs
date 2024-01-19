c91325c22c056c1a448e45d81c7dbd4f6cadb324
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2024-01-18 22:16:15 -0300

    Update Import.cs

--- C#/BoasPraticasCsharp/Alura.Adopet.Console/Comandos/Import.cs
+++ C#/BoasPraticasCsharp/Alura.Adopet.Console/Comandos/Import.cs
@@ -1,20 +1,21 @@
 ﻿using System.Net.Http.Headers;
 using System.Net.Http.Json;
 using Alura.Adopet.Console.Modelos;
+using Alura.Adopet.Console.Servicos;
+using Alura.Adopet.Console.Util;
 
 namespace Alura.Adopet.Console.Comandos
 {
     [DocComando(instrucao: "import",
         documentacao: "adopet import <arquivo> comando que realiza a importação do arquivo de pets.")]
-    public class Import
+    public class Import : IComando
     {
-        HttpClient client;
-
-        public Import()
+        public async Task ExecutarAsync(string[] args)
         {
-            client = new Util().ConfiguraHttpClient("http://localhost:5057");
+            await this.ImportacaoArquivoPetAsync(caminhoDoArquivoDeImportacao: args[1]);
         }
-        public async Task ImportacaoArquivoPetAsync(string caminhoDoArquivoDeImportacao)
+
+        private async Task ImportacaoArquivoPetAsync(string caminhoDoArquivoDeImportacao)
         {
             LeitorDeArquivo leitor = new LeitorDeArquivo();
             List<Pet> listaDePet = leitor.RealizaLeitura(caminhoDoArquivoDeImportacao);
@@ -36,6 +37,7 @@ namespace Alura.Adopet.Console.Comandos
             HttpResponseMessage? response = null;
             using (response = new HttpResponseMessage())
             {
+                var client = new HttpClientPet().ConfiguraHttpClient("http://localhost:5057");
                 return client.PostAsJsonAsync("pet/add", pet);
             }
         }
