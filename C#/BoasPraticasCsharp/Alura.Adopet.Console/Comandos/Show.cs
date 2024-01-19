f584a36625170829a1ed90dc00ee276073dee3df
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2024-01-18 22:16:20 -0300

    Update Show.cs

--- C#/BoasPraticasCsharp/Alura.Adopet.Console/Comandos/Show.cs
+++ C#/BoasPraticasCsharp/Alura.Adopet.Console/Comandos/Show.cs
@@ -1,10 +1,18 @@
-﻿namespace Alura.Adopet.Console.Comandos
+﻿using Alura.Adopet.Console.Util;
+
+namespace Alura.Adopet.Console.Comandos
 {
     [DocComando(instrucao: "show",
                 documentacao: "adopet show < arquivo > comando que exibe no terminal o conteúdo do arquivo importado.")]
-    public class Show
+    public class Show : IComando
     {
-        public void MostrarPetsASerImportado(string caminhoDoArquivoASerExibido)
+        public Task ExecutarAsync(string[] args)
+        {
+            this.MostrarPetsASerImportado(caminhoDoArquivoASerExibido: args[1]);
+            return Task.CompletedTask;
+        }
+
+        private void MostrarPetsASerImportado(string caminhoDoArquivoASerExibido)
         {
             LeitorDeArquivo leitor = new LeitorDeArquivo();
             var listaDePets = leitor.RealizaLeitura(caminhoDoArquivoASerExibido);
