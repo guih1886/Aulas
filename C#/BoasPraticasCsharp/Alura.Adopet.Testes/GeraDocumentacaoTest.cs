3e1d0f920d46fab5fd13d35970869c277bab4362
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2024-01-19 23:42:59 -0300

    Create GeraDocumentacaoTest.cs

---
+++ C#/BoasPraticasCsharp/Alura.Adopet.Testes/GeraDocumentacaoTest.cs
@@ -0,0 +1,22 @@
+﻿using Alura.Adopet.Console.Comandos;
+using System.Reflection;
+using Xunit;
+
+namespace Alura.Adopet.Testes
+{
+    public class GeraDocumentacaoTest
+    {
+        [Fact]
+        public void QuandoExistemComandosDeveRetornarDicionarioNaoVazio()
+        {
+            //Arrange
+            var assemblyComODocComando = Assembly.GetAssembly(typeof(DocComando));
+            //Act
+            Dictionary<string, DocComando> dicionario = DocumentacaoDoSistema.ToDictionary(assemblyComODocComando);
+            //Assert
+            Assert.NotNull(dicionario);
+            Assert.NotEmpty(dicionario);
+            Assert.Equal(3, dicionario.Count);
+        }
+    }
+}
