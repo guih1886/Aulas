baf40f9b5f7ef0ce6f368140da2ba0179b958889
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2024-01-19 23:42:45 -0300

    Update HttpClientPet.cs

--- C#/BoasPraticasCsharp/Alura.Adopet.Console/Servicos/HttpClientPet.cs
+++ C#/BoasPraticasCsharp/Alura.Adopet.Console/Servicos/HttpClientPet.cs
@@ -1,9 +1,16 @@
-﻿using System.Net.Http.Headers;
+﻿using Alura.Adopet.Console.Modelos;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
 
 namespace Alura.Adopet.Console.Servicos
 {
     public class HttpClientPet
     {
+        private HttpClient client;
+        public HttpClientPet(string uri = "http://localhost:60518")
+        {
+            client = ConfiguraHttpClient(uri);
+        }
         public HttpClient ConfiguraHttpClient(string url)
         {
             HttpClient _client = new HttpClient();
@@ -12,6 +19,12 @@ namespace Alura.Adopet.Console.Servicos
                 new MediaTypeWithQualityHeaderValue("application/json"));
             _client.BaseAddress = new Uri(url);
             return _client;
+        }
+
+        public async Task<IEnumerable<Pet>?> ListPetsAsync()
+        {
+            HttpResponseMessage response = await client.GetAsync("pet/list");
+            return await response.Content.ReadFromJsonAsync<IEnumerable<Pet>>();
         }
     }
 }
