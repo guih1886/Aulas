d6934a6d7f7f31bb2ba873ced8b6767486a8095a
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2024-01-19 23:42:49 -0300

    Create DocumentacaoDoSistema.cs

---
+++ C#/BoasPraticasCsharp/Alura.Adopet.Console/Util/DocumentacaoDoSistema.cs
@@ -0,0 +1,16 @@
+﻿using Alura.Adopet.Console.Comandos;
+using System.Reflection;
+
+namespace Alura.Adopet.Testes
+{
+    public class DocumentacaoDoSistema
+    {
+        public static Dictionary<string, DocComando> ToDictionary(Assembly? assemblyComODocComando)
+        {
+            return assemblyComODocComando.GetTypes()
+                .Where(t => t.GetCustomAttributes<DocComando>().Any())
+                .Select(t => t.GetCustomAttribute<DocComando>()!)
+                .ToDictionary(d => d.Instrucao);
+        }
+    }
+}
