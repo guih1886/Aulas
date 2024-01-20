9b014dea6c175a1d9012c11a5e285e376c15705e
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2024-01-19 23:42:42 -0300

    Update List.cs

--- C#/BoasPraticasCsharp/Alura.Adopet.Console/Comandos/List.cs
+++ C#/BoasPraticasCsharp/Alura.Adopet.Console/Comandos/List.cs
@@ -15,18 +15,11 @@ namespace Alura.Adopet.Console.Comandos
 
         private async Task ListarPetsAsync()
         {
-            var pets = await ListPetsAsync();
+            var pets = await new HttpClientPet().ListPetsAsync();
             foreach (var pet in pets)
             {
                 System.Console.WriteLine(pet);
             }
-        }
-
-        private async Task<IEnumerable<Pet>?> ListPetsAsync()
-        {
-            var client = new HttpClientPet().ConfiguraHttpClient("http://localhost:5057");
-            HttpResponseMessage response = await client.GetAsync("pet/list");
-            return await response.Content.ReadFromJsonAsync<IEnumerable<Pet>>();
         }
     }
 }
