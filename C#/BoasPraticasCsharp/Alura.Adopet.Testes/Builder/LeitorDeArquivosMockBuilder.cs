eaf3aba1634fbd0869081d9890acdcf5e615f85f
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2024-01-22 21:06:32 -0300

    Create LeitorDeArquivosMockBuilder.cs

---
+++ C#/BoasPraticasCsharp/Alura.Adopet.Testes/Builder/LeitorDeArquivosMockBuilder.cs
@@ -0,0 +1,17 @@
+﻿using Alura.Adopet.Console.Modelos;
+using Alura.Adopet.Console.Util;
+using Moq;
+
+namespace Alura.Adopet.Testes.Builder
+{
+    public static class LeitorDeArquivosMockBuilder
+    {
+        public static Mock<LeitorDeArquivo> CriaMock(List<Pet> pets)
+        {
+            var leitorDeArquivo = new Mock<LeitorDeArquivo>(MockBehavior.Default, It.IsAny<string>());
+            leitorDeArquivo.Setup(_ => _.RealizaLeitura()).Returns(pets);
+
+            return leitorDeArquivo;
+        }
+    }
+}
