2a2381003a4aca914e0e446a6d0d25285d280245
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2024-01-22 21:06:35 -0300

    Create ImportIntegrationTest.cs

---
+++ C#/BoasPraticasCsharp/Alura.Adopet.Testes/ImportIntegrationTest.cs
@@ -0,0 +1,30 @@
+﻿using Alura.Adopet.Console.Comandos;
+using Alura.Adopet.Console.Modelos;
+using Alura.Adopet.Console.Servicos;
+using Alura.Adopet.Testes.Builder;
+using Xunit;
+
+namespace Alura.Adopet.Testes
+{
+    public class ImportIntegrationTest
+    {
+        [Fact]
+        public async void QuandoApiEstaNoArDeveRetornarUmaListaDePet()
+        {
+            //Arrange
+            var listaDePet = new List<Pet>();
+            var leitorDeArquivo = LeitorDeArquivosMockBuilder.CriaMock(listaDePet);
+
+            var httpClientPet = new HttpClientPet(new HttpClientFactory().CreateClient());
+            var import = new Import(httpClientPet, leitorDeArquivo.Object);
+
+            string[] args = { "import", "lista.csv" };
+            //Act
+            await import.ExecutarAsync(args);
+            //Assert
+            var listaPet = await httpClientPet.ListPetsAsync();
+            Assert.NotNull(listaPet);
+            Assert.NotEmpty(listaPet);
+        }
+    }
+}
