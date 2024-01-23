fd8d71d303c48b790cf6f720bfbe28c2bdfc1840
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2024-01-22 21:06:27 -0300

    Create HttpClientFactory.cs

---
+++ C#/BoasPraticasCsharp/Alura.Adopet.Console/Servicos/HttpClientFactory.cs
@@ -0,0 +1,19 @@
+﻿using System.Net.Http.Headers;
+using System;
+
+namespace Alura.Adopet.Console.Servicos
+{
+    public class HttpClientFactory : IHttpClientFactory
+    {
+        private string url = "http://localhost:60518";
+        public HttpClient CreateClient(string? name)
+        {
+            HttpClient _client = new HttpClient();
+            _client.DefaultRequestHeaders.Accept.Clear();
+            _client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+            _client.BaseAddress = new Uri(url);
+            return _client;
+        }
+    }
+}
