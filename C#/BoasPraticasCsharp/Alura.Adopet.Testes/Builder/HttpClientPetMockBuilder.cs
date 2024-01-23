d0c8c1bd9695508aa5badc38a9f79e6a9173628f
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2024-01-22 21:06:30 -0300

    Create HttpClientPetMockBuilder.cs

---
+++ C#/BoasPraticasCsharp/Alura.Adopet.Testes/Builder/HttpClientPetMockBuilder.cs
@@ -0,0 +1,18 @@
+﻿using Alura.Adopet.Console.Servicos;
+using Moq;
+
+namespace Alura.Adopet.Testes.Builder
+{
+    public static class HttpClientPetMockBuilder
+    {
+        public static Mock<HttpClientPet> GetMock()
+        {
+            var httpClientPet = new Mock<HttpClientPet>(MockBehavior.Default,
+                It.IsAny<HttpClient>());
+
+            return httpClientPet;
+
+            ;
+        }
+    }
+}
