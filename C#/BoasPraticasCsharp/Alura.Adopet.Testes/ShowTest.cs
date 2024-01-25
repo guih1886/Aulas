ecd7a4caac170f673b7b5fa32b8dd5effafac9c2
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2024-01-25 20:32:02 -0300

    Create ShowTest.cs

---
+++ C#/BoasPraticasCsharp/Alura.Adopet.Testes/ShowTest.cs
@@ -0,0 +1,28 @@
+﻿using Alura.Adopet.Console.Comandos;
+using Alura.Adopet.Console.Modelos;
+using Alura.Adopet.Console.Util;
+using Alura.Adopet.Testes.Builder;
+using Xunit;
+
+namespace Alura.Adopet.Testes
+{
+    public class ShowTest
+    {
+        [Fact]
+        public async Task QuandoArquivoExistenteDeveRetornarMensagemDeSucesso()
+        {
+            //Arrange
+            List<Pet> listaDePet = new List<Pet>();
+            Pet pet = new Pet(new Guid(), "Adolfo", TipoPet.Gato);
+            listaDePet.Add(pet);
+            var leitor = LeitorDeArquivosMockBuilder.CriaMock(listaDePet);
+            leitor.Setup(_ => _.RealizaLeitura());
+            //Act
+            var resultado = await new Show(leitor.Object).ExecutarAsync();
+            var sucesso = (SucessWithPets) resultado.Successes[0];
+            //Assert
+            Assert.NotNull(resultado);
+            Assert.Equal("Leitura realizada com sucesso.", sucesso.Mensagem);
+        }
+    }
+}
