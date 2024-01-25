32d68c768ca943bec158f75c0eaf2710662824bb
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2024-01-24 23:12:19 -0300

    Create ConsoleUI.cs

---
+++ C#/BoasPraticasCsharp/Alura.Adopet.Console/UI/ConsoleUI.cs
@@ -0,0 +1,70 @@
+﻿using Alura.Adopet.Console.Util;
+using FluentResults;
+
+namespace Alura.Adopet.Console.UI
+{
+    public static class ConsoleUI
+    {
+        public static void ExibeResultado(Result resultado)
+        {
+            System.Console.ForegroundColor = ConsoleColor.Green;
+            try
+            {
+                if (resultado.IsFailed)
+                {
+                    ExibeFalha(resultado);
+                }
+                else
+                {
+                    ExibeSucesso(resultado);
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            System.Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        private static void ExibeSucesso(Result resultado)
+        {
+            var sucesso = resultado.Successes.First();
+            switch (sucesso)
+            {
+                case SucessWithPets s:
+                    ExibirPets(s);
+                    break;
+                case SucessWithDocs d:
+                    ExibeDocumentacao(d);
+                    break;
+            }
+        }
+
+        private static void ExibeDocumentacao(SucessWithDocs documentacaoComando)
+        {
+            System.Console.WriteLine("Comando possíveis: ");
+            foreach (var doc in documentacaoComando.Documentacao)
+            {
+                System.Console.WriteLine(doc);
+            }
+
+        }
+
+        private static void ExibirPets(SucessWithPets sucesso)
+        {
+            foreach (var pet in sucesso.Data)
+            {
+                System.Console.WriteLine(pet);
+            }
+            System.Console.WriteLine(sucesso.Mensagem);
+        }
+
+        private static void ExibeFalha(Result resultado)
+        {
+            System.Console.ForegroundColor = ConsoleColor.Red;
+            var erro = resultado.Errors[0];
+            System.Console.WriteLine($"Aconteceu um exceção: {erro.Message}");
+        }
+    }
+}
