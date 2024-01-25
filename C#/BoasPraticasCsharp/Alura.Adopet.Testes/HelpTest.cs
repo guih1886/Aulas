a32aa0c96b7188c7bad1a9c4226468191b7efd89
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2024-01-25 20:31:59 -0300

    Create HelpTest.cs

---
+++ C#/BoasPraticasCsharp/Alura.Adopet.Testes/HelpTest.cs
@@ -0,0 +1,33 @@
+﻿using Alura.Adopet.Console.Comandos;
+using Xunit;
+
+namespace Alura.Adopet.Testes
+{
+    public class HelpTest
+    {
+        [Fact]
+        public async Task QuandoMetodoNaoExistirDeveRetornarFalha()
+        {
+            //Arrange
+            var comando = "comando invalido";
+            //Act
+            //Assert
+            await Assert.ThrowsAsync<ArgumentException>(async () => await new Help(comando).ExecutarAsync());
+        }
+
+        [Theory]
+        [InlineData("help")]
+        [InlineData("show")]
+        [InlineData("list")]
+        [InlineData("import")]
+        public async Task QuandoComandoExistirDeveRetornarSucesso(string comando)
+        {
+            //Arrange
+            //Act
+            var resultado = await new Help(comando).ExecutarAsync();
+            //Assert
+            Assert.NotNull(resultado);
+            Assert.True(resultado.IsSuccess);
+        }
+    }
+}
