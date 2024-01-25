0c897e1d02beb07d68febb707ab9867f210735de
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2024-01-25 20:31:54 -0300

    Create ComandoFactoryTest.cs

---
+++ C#/BoasPraticasCsharp/Alura.Adopet.Testes/ComandoFactoryTest.cs
@@ -0,0 +1,51 @@
+﻿using Alura.Adopet.Console.Comandos;
+using Xunit;
+
+namespace Alura.Adopet.Testes
+{
+    public class ComandoFactoryTest
+    {
+        [Fact]
+        public void DadoUmParametroDeveRetornarUmTipoImport()
+        {
+            //Arrange
+            string[] args = { "import", "lista.csv" };
+            //Act
+            var comando = ComandoFactory.CriarComando(args);
+            //Assert
+            Assert.IsType<Import>(comando);
+        }
+
+        [Fact]
+        public void DadoUmParametroInvalidoDeveRetornarNulo()
+        {
+            //Arrange
+            string[] args = { "camarguinho", "lista.csv" };
+            //Act
+            var comando = ComandoFactory.CriarComando(args);
+            //Assert
+            Assert.Null(comando);
+        }
+
+        [Fact]
+        public void DadoUmParametroNuloDeveRetornarNulo()
+        {
+            //Arrange
+            //Act
+            var comando = ComandoFactory.CriarComando(null);
+            //Assert
+            Assert.Null(comando);
+        }
+
+        [Fact]
+        public void DadoUmParametroVazioDeveRetornarNulo()
+        {
+            //Arrange
+            string[] args = { };
+            //Act
+            var comando = ComandoFactory.CriarComando(args);
+            //Assert
+            Assert.Null(comando);
+        }
+    }
+}
