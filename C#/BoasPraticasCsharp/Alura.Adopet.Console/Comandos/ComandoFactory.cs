281277255ec596f838cf58a3d6880b164b176e95
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2024-01-25 20:31:51 -0300

    Create ComandoFactory.cs

---
+++ C#/BoasPraticasCsharp/Alura.Adopet.Console/Comandos/ComandoFactory.cs
@@ -0,0 +1,34 @@
+﻿using Alura.Adopet.Console.Servicos;
+using Alura.Adopet.Console.Util;
+
+namespace Alura.Adopet.Console.Comandos
+{
+    public static class ComandoFactory
+    {
+        public static IComando? CriarComando(string[] args)
+        {
+            if (args is null || args.Length == 0)
+            {
+                return null;
+            }
+            var comando = args[0];
+            switch (comando)
+            {
+                case "import":
+                    var httpClientPet = new HttpClientPet(new HttpClientFactory().CreateClient());
+                    LeitorDeArquivo? leitorDeArquivos = new LeitorDeArquivo(comando);
+                    return new Import(httpClientPet, leitorDeArquivos);
+                case "list":
+                    var httpClientPetList = new HttpClientPet(new HttpClientFactory().CreateClient());
+                    return new List(httpClientPetList);
+                case "show":
+                    LeitorDeArquivo? leitorDeArquivosShow = new LeitorDeArquivo(comando);
+                    return new Show(leitorDeArquivosShow);
+                case "help":
+                    var comandoASerExibido = args.Length == 2 ? args[1] : null;
+                    return new Help(comandoASerExibido);
+                default: return null;
+            }
+        }
+    }
+}
