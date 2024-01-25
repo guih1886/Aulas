bcb49a72ef747571b7c7a840ba0cac09d68dc817
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2024-01-24 23:12:26 -0300

    Create SucessWithPets.cs

---
+++ C#/BoasPraticasCsharp/Alura.Adopet.Console/Util/SucessWithPets.cs
@@ -0,0 +1,17 @@
+﻿using Alura.Adopet.Console.Modelos;
+using FluentResults;
+
+namespace Alura.Adopet.Console.Util
+{
+    public class SucessWithPets : Success
+    {
+        public IEnumerable<Pet> Data { get; }
+        public string Mensagem;
+
+        public SucessWithPets(IEnumerable<Pet> data, string msg)
+        {
+            Data = data;
+            Mensagem = msg;
+        }
+    }
+}
