15ce0d0d079a6dfb3c65e6f99384580e5eb706cc
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2024-01-24 23:12:21 -0300

    Create SucessWithDocs.cs

---
+++ C#/BoasPraticasCsharp/Alura.Adopet.Console/Util/SucessWithDocs.cs
@@ -0,0 +1,14 @@
+﻿using FluentResults;
+
+namespace Alura.Adopet.Console.Util
+{
+    public class SucessWithDocs : Success
+    {
+        public IEnumerable<string> Documentacao { get; }
+
+        public SucessWithDocs(IEnumerable<string> documentacao)
+        {
+            Documentacao = documentacao;
+        }
+    }
+}
