19ef52c77016222a61ab80923c7a69de3ac87067
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2024-01-29 21:46:33 -0300

    Create ClientesDoCsv.cs

---
+++ C#/BoasPraticasCsharp/Alura.Adopet.Console/Servicos/Arquivos/ClientesDoCsv.cs
@@ -0,0 +1,22 @@
+﻿using Alura.Adopet.Console.Modelos;
+using Alura.Adopet.Console.Servicos.Abstracoes;
+
+namespace Alura.Adopet.Console.Servicos.Arquivos
+{
+    public class ClientesDoCsv : LeitorDeArquivoCsv<Cliente>
+    {
+        public ClientesDoCsv(string? caminhoArquivo) : base(caminhoArquivo)
+        {
+        }
+
+        public override Cliente CriarDaLinhaCsv(string linha)
+        {
+            string[] propriedades = linha.Split(';');
+            return new Cliente(
+                id: Guid.Parse(propriedades[0]),
+                nome: propriedades[1],
+                email: propriedades[2]
+                );
+        }
+    }
+}
