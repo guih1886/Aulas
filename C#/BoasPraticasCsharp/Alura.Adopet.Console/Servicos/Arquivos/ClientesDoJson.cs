407f75ac634aa3bcc6fc7792c0f0260dd388f034
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2024-01-29 21:46:31 -0300

    Create ClientesDoJson.cs

---
+++ C#/BoasPraticasCsharp/Alura.Adopet.Console/Servicos/Arquivos/ClientesDoJson.cs
@@ -0,0 +1,11 @@
+﻿using Alura.Adopet.Console.Modelos;
+
+namespace Alura.Adopet.Console.Servicos.Arquivos
+{
+    public class ClientesDoJson : LeitorDeArquivosJson<Cliente>
+    {
+        public ClientesDoJson(string caminhoArquivo) : base(caminhoArquivo)
+        {
+        }
+    }
+}
