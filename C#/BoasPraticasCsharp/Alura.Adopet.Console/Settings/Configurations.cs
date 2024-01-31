8fd7d9e6ebd0373b6ccd9617398163b8872b660d
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2024-01-30 23:29:38 -0300

    Create Configurations.cs

---
+++ C#/BoasPraticasCsharp/Alura.Adopet.Console/Settings/Configurations.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Alura.Adopet.Console.Settings
+{
+    public static class Configurations
+    {
+        public static IConfiguration BuildConfiguration()
+        {
+            return new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .Build();
+        }
+
+        public static AppSettings ApiSetting
+        {
+            get
+            {
+                var _config = BuildConfiguration();
+                return _config
+                    .GetSection(AppSettings.Section)
+                    .Get<AppSettings>() ??
+                    throw new ArgumentException("Seção para configuração da API não encontrada!");
+            }
+        }
+    }
+}
