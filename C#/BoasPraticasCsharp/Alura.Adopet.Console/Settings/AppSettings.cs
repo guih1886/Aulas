cb91222c6b8d199e09bac1efbebe18597a58cd5a
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2024-01-30 23:29:35 -0300

    Create AppSettings.cs

---
+++ C#/BoasPraticasCsharp/Alura.Adopet.Console/Settings/AppSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace Alura.Adopet.Console.Settings
+{
+    public class AppSettings
+    {
+        public const string Section = "AdopetApi";
+        public string Uri { get; set; } = string.Empty;
+    }
+}
