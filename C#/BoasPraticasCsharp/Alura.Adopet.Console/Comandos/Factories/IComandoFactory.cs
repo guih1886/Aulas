7f68aa5eac79be4fa7804b69f00078568a3430a0
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2024-01-30 22:45:03 -0300

    Create IComandoFactory.cs

---
+++ C#/BoasPraticasCsharp/Alura.Adopet.Console/Comandos/Factories/IComandoFactory.cs
@@ -0,0 +1,8 @@
+﻿namespace Alura.Adopet.Console.Comandos.Factories
+{
+    public interface IComandoFactory
+    {
+        bool ConsegueCriarOTipo(Type? tipoComando);
+        IComando? CriarComando(string[] args);
+    }
+}
