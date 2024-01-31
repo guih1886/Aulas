4d32047ff813b7042599e6ed683023d20de8db2a
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2024-01-30 22:44:59 -0300

    Create ListFactory.cs

---
+++ C#/BoasPraticasCsharp/Alura.Adopet.Console/Comandos/Factories/ListFactory.cs
@@ -0,0 +1,18 @@
+﻿using Alura.Adopet.Console.Servicos.Http;
+
+namespace Alura.Adopet.Console.Comandos.Factories
+{
+    public class ListFactory : IComandoFactory
+    {
+        public bool ConsegueCriarOTipo(Type? tipoComando)
+        {
+            return tipoComando?.IsAssignableTo(typeof(List)) ?? false;
+        }
+
+        public IComando? CriarComando(string[] args)
+        {
+            var httpClientPetList = new PetService(new HttpClientFactory().CreateClient());
+            return new List(httpClientPetList);
+        }
+    }
+}
