0d80e1531b0779436386f028ffad95d101050bf2
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2024-02-02 22:54:09 -0300

    create project
    create project

---
+++ C#/microservices-dotnet-Ini/RestauranteService/ItemServiceHttpClient/ItemServiceHttpClient.cs
@@ -0,0 +1,28 @@
+﻿using RestauranteService.Dtos;
+using System.Text;
+using System.Text.Json;
+
+namespace RestauranteService.ItemServiceHttpClient
+{
+    public class ItemServiceHttpClient : IItemServiceHttpClient
+    {
+        private readonly HttpClient client;
+        public readonly IConfiguration _configuration;
+
+        public ItemServiceHttpClient(HttpClient client, IConfiguration configuration)
+        {
+            this.client = client;
+            _configuration = configuration;
+        }
+        public async void EnviaRestauranteParaItemService(RestauranteReadDto readDto)
+        {
+            var conteudoHttp = new StringContent
+                (
+                    JsonSerializer.Serialize(readDto),
+                    Encoding.UTF8,
+                    "application/json"
+                );
+            await client.PostAsync("http://localhost:5000/api/item/Restaurante", conteudoHttp);
+        }
+    }
+}
