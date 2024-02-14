c04387fb0a658b4a1c58580623b9c7d76bf540aa
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2024-02-14 19:42:43 -0300

    form menu creation
    form menu creation

---
+++ C#/WindowsForms/Curso/CursoWindowsForms/CursoWindowsForms/Form_Menu.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace CursoWindowsForms
+{
+    public partial class Form_Menu : Form
+    {
+        public Form_Menu()
+        {
+            InitializeComponent();
+        }
+
+        private void sairToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
