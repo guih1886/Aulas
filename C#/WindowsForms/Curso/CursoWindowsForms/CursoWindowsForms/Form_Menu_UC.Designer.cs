186784c641cafc07fb75c9197f466f294d12a123
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2024-02-15 23:36:03 -0300

    form menu user control creation
    form menu user control creation

---
+++ C#/WindowsForms/Curso/CursoWindowsForms/CursoWindowsForms/Form_Menu_UC.Designer.cs
@@ -0,0 +1,257 @@
+﻿namespace CursoWindowsForms
+{
+    partial class Form_Menu_UC
+    {
+        /// <summary> 
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Designer de Componentes
+
+        /// <summary> 
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form_Menu_UC));
+            this.Mnu_Principal = new System.Windows.Forms.MenuStrip();
+            this.arquivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.novoToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.demonstraçãoKeyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.máscaraToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.validaCPFToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.validaCPF2ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.validaSenhaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sairToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.windowsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cascataToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.horizontalToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.verticalToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sobreToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.Tuc_Aplicacao = new System.Windows.Forms.TabControl();
+            this.Iml_Imagens = new System.Windows.Forms.ImageList(this.components);
+            this.fecharAbaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.Mnu_Principal.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Mnu_Principal
+            // 
+            this.Mnu_Principal.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.arquivoToolStripMenuItem,
+            this.windowsToolStripMenuItem,
+            this.sobreToolStripMenuItem});
+            this.Mnu_Principal.Location = new System.Drawing.Point(0, 0);
+            this.Mnu_Principal.Name = "Mnu_Principal";
+            this.Mnu_Principal.Size = new System.Drawing.Size(800, 24);
+            this.Mnu_Principal.TabIndex = 0;
+            this.Mnu_Principal.Text = "menuStrip1";
+            // 
+            // arquivoToolStripMenuItem
+            // 
+            this.arquivoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.novoToolStripMenuItem1,
+            this.sairToolStripMenuItem1});
+            this.arquivoToolStripMenuItem.Name = "arquivoToolStripMenuItem";
+            this.arquivoToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.arquivoToolStripMenuItem.Text = "Arquivo";
+            // 
+            // novoToolStripMenuItem1
+            // 
+            this.novoToolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.demonstraçãoKeyToolStripMenuItem,
+            this.máscaraToolStripMenuItem,
+            this.validaCPFToolStripMenuItem,
+            this.validaCPF2ToolStripMenuItem,
+            this.validaSenhaToolStripMenuItem});
+            this.novoToolStripMenuItem1.Name = "novoToolStripMenuItem1";
+            this.novoToolStripMenuItem1.Size = new System.Drawing.Size(103, 22);
+            this.novoToolStripMenuItem1.Text = "Novo";
+            // 
+            // demonstraçãoKeyToolStripMenuItem
+            // 
+            this.demonstraçãoKeyToolStripMenuItem.Image = global::CursoWindowsForms.Properties.Resources.Frm_DemonstracaoKey;
+            this.demonstraçãoKeyToolStripMenuItem.Name = "demonstraçãoKeyToolStripMenuItem";
+            this.demonstraçãoKeyToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.K)));
+            this.demonstraçãoKeyToolStripMenuItem.Size = new System.Drawing.Size(214, 22);
+            this.demonstraçãoKeyToolStripMenuItem.Text = "Demonstração &Key";
+            this.demonstraçãoKeyToolStripMenuItem.Click += new System.EventHandler(this.demonstraçãoKeyToolStripMenuItem_Click);
+            // 
+            // máscaraToolStripMenuItem
+            // 
+            this.máscaraToolStripMenuItem.Image = global::CursoWindowsForms.Properties.Resources.Frm_Mascara;
+            this.máscaraToolStripMenuItem.Name = "máscaraToolStripMenuItem";
+            this.máscaraToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.M)));
+            this.máscaraToolStripMenuItem.Size = new System.Drawing.Size(214, 22);
+            this.máscaraToolStripMenuItem.Text = "&Máscara";
+            this.máscaraToolStripMenuItem.Click += new System.EventHandler(this.máscaraToolStripMenuItem_Click);
+            // 
+            // validaCPFToolStripMenuItem
+            // 
+            this.validaCPFToolStripMenuItem.Image = global::CursoWindowsForms.Properties.Resources.Frm_ValidaCPF;
+            this.validaCPFToolStripMenuItem.Name = "validaCPFToolStripMenuItem";
+            this.validaCPFToolStripMenuItem.Size = new System.Drawing.Size(214, 22);
+            this.validaCPFToolStripMenuItem.Text = "Valida CPF";
+            this.validaCPFToolStripMenuItem.Click += new System.EventHandler(this.validaCPFToolStripMenuItem_Click);
+            // 
+            // validaCPF2ToolStripMenuItem
+            // 
+            this.validaCPF2ToolStripMenuItem.Image = global::CursoWindowsForms.Properties.Resources.Frm_ValidaCPF2;
+            this.validaCPF2ToolStripMenuItem.Name = "validaCPF2ToolStripMenuItem";
+            this.validaCPF2ToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.C)));
+            this.validaCPF2ToolStripMenuItem.Size = new System.Drawing.Size(214, 22);
+            this.validaCPF2ToolStripMenuItem.Text = "Valida &CPF 2";
+            this.validaCPF2ToolStripMenuItem.Click += new System.EventHandler(this.validaCPF2ToolStripMenuItem_Click);
+            // 
+            // validaSenhaToolStripMenuItem
+            // 
+            this.validaSenhaToolStripMenuItem.Image = global::CursoWindowsForms.Properties.Resources.Frm_ValidaSenha;
+            this.validaSenhaToolStripMenuItem.Name = "validaSenhaToolStripMenuItem";
+            this.validaSenhaToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
+            this.validaSenhaToolStripMenuItem.Size = new System.Drawing.Size(214, 22);
+            this.validaSenhaToolStripMenuItem.Text = "Valida &Senha";
+            this.validaSenhaToolStripMenuItem.Click += new System.EventHandler(this.validaSenhaToolStripMenuItem_Click);
+            // 
+            // sairToolStripMenuItem1
+            // 
+            this.sairToolStripMenuItem1.Name = "sairToolStripMenuItem1";
+            this.sairToolStripMenuItem1.Size = new System.Drawing.Size(103, 22);
+            this.sairToolStripMenuItem1.Text = "Sair";
+            this.sairToolStripMenuItem1.Click += new System.EventHandler(this.sairToolStripMenuItem1_Click);
+            // 
+            // windowsToolStripMenuItem
+            // 
+            this.windowsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cascataToolStripMenuItem,
+            this.horizontalToolStripMenuItem,
+            this.verticalToolStripMenuItem,
+            this.fecharAbaToolStripMenuItem});
+            this.windowsToolStripMenuItem.Name = "windowsToolStripMenuItem";
+            this.windowsToolStripMenuItem.Size = new System.Drawing.Size(68, 20);
+            this.windowsToolStripMenuItem.Text = "Windows";
+            // 
+            // cascataToolStripMenuItem
+            // 
+            this.cascataToolStripMenuItem.Name = "cascataToolStripMenuItem";
+            this.cascataToolStripMenuItem.Size = new System.Drawing.Size(129, 22);
+            this.cascataToolStripMenuItem.Text = "Cascata";
+            this.cascataToolStripMenuItem.Click += new System.EventHandler(this.cascataToolStripMenuItem_Click);
+            // 
+            // horizontalToolStripMenuItem
+            // 
+            this.horizontalToolStripMenuItem.Name = "horizontalToolStripMenuItem";
+            this.horizontalToolStripMenuItem.Size = new System.Drawing.Size(129, 22);
+            this.horizontalToolStripMenuItem.Text = "Horizontal";
+            this.horizontalToolStripMenuItem.Click += new System.EventHandler(this.horizontalToolStripMenuItem_Click);
+            // 
+            // verticalToolStripMenuItem
+            // 
+            this.verticalToolStripMenuItem.Name = "verticalToolStripMenuItem";
+            this.verticalToolStripMenuItem.Size = new System.Drawing.Size(129, 22);
+            this.verticalToolStripMenuItem.Text = "Vertical";
+            this.verticalToolStripMenuItem.Click += new System.EventHandler(this.verticalToolStripMenuItem_Click);
+            // 
+            // sobreToolStripMenuItem
+            // 
+            this.sobreToolStripMenuItem.Name = "sobreToolStripMenuItem";
+            this.sobreToolStripMenuItem.Size = new System.Drawing.Size(49, 20);
+            this.sobreToolStripMenuItem.Text = "Sobre";
+            this.sobreToolStripMenuItem.Click += new System.EventHandler(this.sobreToolStripMenuItem_Click);
+            // 
+            // Tuc_Aplicacao
+            // 
+            this.Tuc_Aplicacao.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Tuc_Aplicacao.HotTrack = true;
+            this.Tuc_Aplicacao.ImageList = this.Iml_Imagens;
+            this.Tuc_Aplicacao.Location = new System.Drawing.Point(0, 24);
+            this.Tuc_Aplicacao.Name = "Tuc_Aplicacao";
+            this.Tuc_Aplicacao.SelectedIndex = 0;
+            this.Tuc_Aplicacao.Size = new System.Drawing.Size(800, 426);
+            this.Tuc_Aplicacao.TabIndex = 2;
+            // 
+            // Iml_Imagens
+            // 
+            this.Iml_Imagens.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("Iml_Imagens.ImageStream")));
+            this.Iml_Imagens.TransparentColor = System.Drawing.Color.Transparent;
+            this.Iml_Imagens.Images.SetKeyName(0, "Frm_DemonstracaoKey.ico");
+            this.Iml_Imagens.Images.SetKeyName(1, "Frm_DemonstracaoKey.png");
+            this.Iml_Imagens.Images.SetKeyName(2, "Frm_HelloWorld.ico");
+            this.Iml_Imagens.Images.SetKeyName(3, "Frm_HelloWorld.png");
+            this.Iml_Imagens.Images.SetKeyName(4, "Frm_Mascara.ico");
+            this.Iml_Imagens.Images.SetKeyName(5, "Frm_Mascara.png");
+            this.Iml_Imagens.Images.SetKeyName(6, "Frm_Principal.ico");
+            this.Iml_Imagens.Images.SetKeyName(7, "Frm_ValidaCPF.ico");
+            this.Iml_Imagens.Images.SetKeyName(8, "Frm_ValidaCPF.png");
+            this.Iml_Imagens.Images.SetKeyName(9, "Frm_ValidaCPF2.ico");
+            this.Iml_Imagens.Images.SetKeyName(10, "Frm_ValidaCPF2.png");
+            this.Iml_Imagens.Images.SetKeyName(11, "Frm_ValidaSenha.ico");
+            this.Iml_Imagens.Images.SetKeyName(12, "Frm_ValidaSenha.png");
+            this.Iml_Imagens.Images.SetKeyName(13, "linkedin.png");
+            this.Iml_Imagens.Images.SetKeyName(14, "senha.png");
+            this.Iml_Imagens.Images.SetKeyName(15, "site.png");
+            // 
+            // fecharAbaToolStripMenuItem
+            // 
+            this.fecharAbaToolStripMenuItem.Name = "fecharAbaToolStripMenuItem";
+            this.fecharAbaToolStripMenuItem.ShortcutKeys = System.Windows.Forms.Keys.Delete;
+            this.fecharAbaToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.fecharAbaToolStripMenuItem.Text = "Fechar Aba";
+            this.fecharAbaToolStripMenuItem.Click += new System.EventHandler(this.fecharAbaToolStripMenuItem_Click);
+            // 
+            // Form_Menu_UC
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Tuc_Aplicacao);
+            this.Controls.Add(this.Mnu_Principal);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.Mnu_Principal;
+            this.Name = "Form_Menu_UC";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Formulário Principal";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Mnu_Principal.ResumeLayout(false);
+            this.Mnu_Principal.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip Mnu_Principal;
+        private System.Windows.Forms.ToolStripMenuItem arquivoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem novoToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem sairToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem windowsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem demonstraçãoKeyToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem máscaraToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem validaCPFToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem validaCPF2ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem validaSenhaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sobreToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cascataToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem horizontalToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem verticalToolStripMenuItem;
+        private System.Windows.Forms.TabControl Tuc_Aplicacao;
+        private System.Windows.Forms.ImageList Iml_Imagens;
+        private System.Windows.Forms.ToolStripMenuItem fecharAbaToolStripMenuItem;
+    }
+}
---
+++ C#/WindowsForms/Curso/CursoWindowsForms/CursoWindowsForms/Form_Menu_UC.cs
@@ -0,0 +1,93 @@
+﻿using CursoWindowsForms.Forms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CursoWindowsForms
+{
+    public partial class Form_Menu_UC : Form
+    {
+        int controleDM = 0;
+        public Form_Menu_UC()
+        {
+            InitializeComponent();
+        }
+        private void sairToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void demonstraçãoKeyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            controleDM++;
+            Form_DesmonstracaoKey_UC u = new Form_DesmonstracaoKey_UC();
+            TabPage tb = new TabPage();
+            tb.Name = "Demonstração Key" + controleDM;
+            tb.Text = "Demonstração Key" + controleDM;
+            tb.ImageIndex = 1;
+            tb.Controls.Add(u);
+            Tuc_Aplicacao.TabPages.Add(tb);
+        }
+
+        private void máscaraToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form form = new Form_Mascara();
+            form.MdiParent = this;
+            form.Show();
+        }
+
+        private void validaCPFToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form form = new Form_ValidaCPF();
+            form.MdiParent = this;
+            form.Show();
+        }
+
+        private void validaCPF2ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form form = new Form_ValidaCPF2();
+            form.MdiParent = this;
+            form.Show();
+        }
+
+        private void validaSenhaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form form = new Form_ValidaSenha();
+            form.MdiParent = this;
+            form.Show();
+        }
+
+        private void sobreToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form form = new Form_Sobre();
+            form.MdiParent = this;
+            form.Show();
+        }
+
+        private void cascataToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.LayoutMdi(MdiLayout.Cascade);
+        }
+
+        private void horizontalToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.LayoutMdi(MdiLayout.TileHorizontal);
+        }
+
+        private void verticalToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.LayoutMdi(MdiLayout.TileVertical);
+        }
+
+        private void fecharAbaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if(Tuc_Aplicacao.SelectedTab != null) Tuc_Aplicacao.TabPages.Remove(Tuc_Aplicacao.SelectedTab);
+        }
+    }
+}
