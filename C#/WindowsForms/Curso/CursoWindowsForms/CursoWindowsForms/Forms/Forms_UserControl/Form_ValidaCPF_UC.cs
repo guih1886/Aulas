68188738ffef65090a55e4b528fdd58301e02586
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2024-02-16 22:22:26 -0300

    form valida cpf UC creation
    form valida cpf UC creation

---
+++ C#/WindowsForms/Curso/CursoWindowsForms/CursoWindowsForms/Forms/Forms_UserControl/Form_ValidaCPF_UC.cs
@@ -0,0 +1,44 @@
+﻿using CursoWindowsFormsBiblioteca;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CursoWindowsForms.Forms.Forms_UserControl
+{
+    public partial class Form_ValidaCPF_UC : UserControl
+    {
+        public Form_ValidaCPF_UC()
+        {
+            InitializeComponent();
+        }
+        private void Btn_Reset_Click(object sender, EventArgs e)
+        {
+            this.Lbl_Resultado.Text = string.Empty;
+            Msk_CPF.Clear();
+            Msk_CPF.Focus();
+        }
+
+        private void Btn_Valida_Click(object sender, EventArgs e)
+        {
+            Msk_CPF.Focus();
+            bool validaCPF = false;
+            validaCPF = Cls_Uteis.Validar(Msk_CPF.Text);
+            if (validaCPF)
+            {
+                Lbl_Resultado.Text = "CPF válido.";
+                Lbl_Resultado.ForeColor = Color.Green;
+            }
+            else
+            {
+                Lbl_Resultado.Text = "CPF inválido.";
+                Lbl_Resultado.ForeColor = Color.Red;
+            }
+        }
+    }
+}
