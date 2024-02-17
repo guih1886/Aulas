8684994a4bd51f111f10e248c586e0034a8ee096
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2024-02-16 22:22:55 -0300

    form arquivo imagem UC creation
    form arquivo imagem UC creation

---
+++ C#/WindowsForms/Curso/CursoWindowsForms/CursoWindowsForms/Forms/Forms_UserControl/Frm_ArquivoImagem.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace CursoWindowsForms.Forms.Forms_UserControl
+{
+    partial class Frm_ArquivoImagem
+    {
+        /// <summary> 
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Designer de Componentes
+
+        /// <summary> 
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.Pic_ArquivoImagem = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Pic_ArquivoImagem)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // Pic_ArquivoImagem
+            // 
+            this.Pic_ArquivoImagem.Location = new System.Drawing.Point(20, 71);
+            this.Pic_ArquivoImagem.Name = "Pic_ArquivoImagem";
+            this.Pic_ArquivoImagem.Size = new System.Drawing.Size(169, 161);
+            this.Pic_ArquivoImagem.TabIndex = 1;
+            this.Pic_ArquivoImagem.TabStop = false;
+            // 
+            // Frm_ArquivoImagem
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.Pic_ArquivoImagem);
+            this.Controls.Add(this.label1);
+            this.Name = "Frm_ArquivoImagem";
+            this.Size = new System.Drawing.Size(444, 258);
+            ((System.ComponentModel.ISupportInitialize)(this.Pic_ArquivoImagem)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox Pic_ArquivoImagem;
+    }
+}
---
+++ C#/WindowsForms/Curso/CursoWindowsForms/CursoWindowsForms/Forms/Forms_UserControl/Frm_ArquivoImagem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CursoWindowsForms.Forms.Forms_UserControl
+{
+    public partial class Frm_ArquivoImagem : UserControl
+    {
+        public Frm_ArquivoImagem()
+        {
+            InitializeComponent();
+        }
+    }
+}
