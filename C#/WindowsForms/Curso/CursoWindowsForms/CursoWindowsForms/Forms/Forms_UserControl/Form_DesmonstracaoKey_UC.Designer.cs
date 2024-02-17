6dea47a1133a3f3a4b5f3fa73e47de42b0ff7940
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2024-02-16 22:21:50 -0300

    form DM key UC creation
    form DM key UC creation

---
+++ C#/WindowsForms/Curso/CursoWindowsForms/CursoWindowsForms/Forms/Forms_UserControl/Form_DesmonstracaoKey_UC.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace CursoWindowsForms.Forms
+{
+    partial class Form_DesmonstracaoKey_UC
+    {
+        /// <summary> 
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Designer de Componentes
+
+        /// <summary> 
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Txt_Input = new System.Windows.Forms.TextBox();
+            this.Txt_Msg = new System.Windows.Forms.TextBox();
+            this.Btn_Reset = new System.Windows.Forms.Button();
+            this.Lbl_Minus = new System.Windows.Forms.Label();
+            this.Lbl_Maius = new System.Windows.Forms.Label();
+            this.Lbl_Upper = new System.Windows.Forms.Label();
+            this.Lbl_Lower = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Txt_Input
+            // 
+            this.Txt_Input.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Txt_Input.Location = new System.Drawing.Point(8, 8);
+            this.Txt_Input.MaximumSize = new System.Drawing.Size(315, 20);
+            this.Txt_Input.MinimumSize = new System.Drawing.Size(315, 20);
+            this.Txt_Input.Name = "Txt_Input";
+            this.Txt_Input.Size = new System.Drawing.Size(315, 20);
+            this.Txt_Input.TabIndex = 0;
+            this.Txt_Input.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Txt_Input_KeyDown);
+            // 
+            // Txt_Msg
+            // 
+            this.Txt_Msg.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Txt_Msg.Location = new System.Drawing.Point(8, 40);
+            this.Txt_Msg.Multiline = true;
+            this.Txt_Msg.Name = "Txt_Msg";
+            this.Txt_Msg.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.Txt_Msg.Size = new System.Drawing.Size(334, 265);
+            this.Txt_Msg.TabIndex = 1;
+            this.Txt_Msg.TabStop = false;
+            // 
+            // Btn_Reset
+            // 
+            this.Btn_Reset.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.Btn_Reset.Location = new System.Drawing.Point(340, 8);
+            this.Btn_Reset.Name = "Btn_Reset";
+            this.Btn_Reset.Size = new System.Drawing.Size(93, 22);
+            this.Btn_Reset.TabIndex = 2;
+            this.Btn_Reset.Text = "Limpar";
+            this.Btn_Reset.UseVisualStyleBackColor = true;
+            this.Btn_Reset.Click += new System.EventHandler(this.Btn_Reset_Click);
+            // 
+            // Lbl_Minus
+            // 
+            this.Lbl_Minus.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.Lbl_Minus.AutoSize = true;
+            this.Lbl_Minus.Location = new System.Drawing.Point(350, 104);
+            this.Lbl_Minus.Name = "Lbl_Minus";
+            this.Lbl_Minus.Size = new System.Drawing.Size(38, 13);
+            this.Lbl_Minus.TabIndex = 3;
+            this.Lbl_Minus.Text = "Minus.";
+            // 
+            // Lbl_Maius
+            // 
+            this.Lbl_Maius.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.Lbl_Maius.AutoSize = true;
+            this.Lbl_Maius.Location = new System.Drawing.Point(350, 56);
+            this.Lbl_Maius.Name = "Lbl_Maius";
+            this.Lbl_Maius.Size = new System.Drawing.Size(41, 13);
+            this.Lbl_Maius.TabIndex = 4;
+            this.Lbl_Maius.Text = "Maius..";
+            // 
+            // Lbl_Upper
+            // 
+            this.Lbl_Upper.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.Lbl_Upper.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.Lbl_Upper.Location = new System.Drawing.Point(398, 56);
+            this.Lbl_Upper.Name = "Lbl_Upper";
+            this.Lbl_Upper.Size = new System.Drawing.Size(32, 23);
+            this.Lbl_Upper.TabIndex = 5;
+            // 
+            // Lbl_Lower
+            // 
+            this.Lbl_Lower.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.Lbl_Lower.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.Lbl_Lower.Location = new System.Drawing.Point(398, 104);
+            this.Lbl_Lower.Name = "Lbl_Lower";
+            this.Lbl_Lower.Size = new System.Drawing.Size(32, 23);
+            this.Lbl_Lower.TabIndex = 6;
+            // 
+            // Form_DesmonstracaoKey_UC
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.Controls.Add(this.Lbl_Lower);
+            this.Controls.Add(this.Lbl_Upper);
+            this.Controls.Add(this.Lbl_Maius);
+            this.Controls.Add(this.Lbl_Minus);
+            this.Controls.Add(this.Btn_Reset);
+            this.Controls.Add(this.Txt_Msg);
+            this.Controls.Add(this.Txt_Input);
+            this.MinimumSize = new System.Drawing.Size(455, 356);
+            this.Name = "Form_DesmonstracaoKey_UC";
+            this.Size = new System.Drawing.Size(455, 356);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Txt_Input;
+        private System.Windows.Forms.TextBox Txt_Msg;
+        private System.Windows.Forms.Button Btn_Reset;
+        private System.Windows.Forms.Label Lbl_Minus;
+        private System.Windows.Forms.Label Lbl_Maius;
+        private System.Windows.Forms.Label Lbl_Upper;
+        private System.Windows.Forms.Label Lbl_Lower;
+    }
+}
