a3ed102f6572469163ad08fcab0a56d8a350c498
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2024-02-16 22:22:37 -0300

    form valida senha UC creation
    form valida senha UC creation

---
+++ C#/WindowsForms/Curso/CursoWindowsForms/CursoWindowsForms/Forms/Forms_UserControl/Form_ValidaSenha_UC.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace CursoWindowsForms.Forms.Forms_UserControl
+{
+    partial class Form_ValidaSenha_UC
+    {
+        /// <summary> 
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Designer de Componentes
+
+        /// <summary> 
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Btn_MostrarSenha = new System.Windows.Forms.Button();
+            this.Btn_Reset = new System.Windows.Forms.Button();
+            this.Lbl_Resultado = new System.Windows.Forms.Label();
+            this.Txt_Senha = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Btn_MostrarSenha
+            // 
+            this.Btn_MostrarSenha.Location = new System.Drawing.Point(245, 46);
+            this.Btn_MostrarSenha.Name = "Btn_MostrarSenha";
+            this.Btn_MostrarSenha.Size = new System.Drawing.Size(111, 23);
+            this.Btn_MostrarSenha.TabIndex = 7;
+            this.Btn_MostrarSenha.Text = "Mostrar Senha";
+            this.Btn_MostrarSenha.UseVisualStyleBackColor = true;
+            this.Btn_MostrarSenha.Click += new System.EventHandler(this.Btn_MostrarSenha_MouseLeave);
+            // 
+            // Btn_Reset
+            // 
+            this.Btn_Reset.Location = new System.Drawing.Point(245, 16);
+            this.Btn_Reset.Name = "Btn_Reset";
+            this.Btn_Reset.Size = new System.Drawing.Size(111, 23);
+            this.Btn_Reset.TabIndex = 6;
+            this.Btn_Reset.Text = "Limpar";
+            this.Btn_Reset.UseVisualStyleBackColor = true;
+            this.Btn_Reset.Click += new System.EventHandler(this.Btn_Reset_Click);
+            // 
+            // Lbl_Resultado
+            // 
+            this.Lbl_Resultado.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lbl_Resultado.Location = new System.Drawing.Point(12, 46);
+            this.Lbl_Resultado.Name = "Lbl_Resultado";
+            this.Lbl_Resultado.Size = new System.Drawing.Size(218, 20);
+            this.Lbl_Resultado.TabIndex = 5;
+            // 
+            // Txt_Senha
+            // 
+            this.Txt_Senha.Location = new System.Drawing.Point(12, 19);
+            this.Txt_Senha.Name = "Txt_Senha";
+            this.Txt_Senha.PasswordChar = '*';
+            this.Txt_Senha.Size = new System.Drawing.Size(218, 20);
+            this.Txt_Senha.TabIndex = 4;
+            // 
+            // Form_ValidaSenha_UC
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.Btn_MostrarSenha);
+            this.Controls.Add(this.Btn_Reset);
+            this.Controls.Add(this.Lbl_Resultado);
+            this.Controls.Add(this.Txt_Senha);
+            this.Name = "Form_ValidaSenha_UC";
+            this.Size = new System.Drawing.Size(366, 96);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Btn_MostrarSenha;
+        private System.Windows.Forms.Button Btn_Reset;
+        private System.Windows.Forms.Label Lbl_Resultado;
+        private System.Windows.Forms.TextBox Txt_Senha;
+    }
+}
