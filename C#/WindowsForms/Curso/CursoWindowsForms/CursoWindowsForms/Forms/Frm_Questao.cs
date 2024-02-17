6320c6f70a42a34bccb71be55af38364530be4cb
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2024-02-16 22:23:13 -0300

    form question creation
    form question creation

---
+++ C#/WindowsForms/Curso/CursoWindowsForms/CursoWindowsForms/Forms/Frm_Questao.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace CursoWindowsForms.Forms
+{
+    public partial class Frm_Questao : Form
+    {
+        public Frm_Questao(string nomeImagem, string titulo, string mensagem)
+        {
+            InitializeComponent();
+            Image imagem = (Image)global::CursoWindowsForms.Properties.Resources.ResourceManager.GetObject(nomeImagem);
+            Pic_Imagem.Image = imagem;
+            Lbl_Questao.Text = mensagem;
+            Frm_Questao.ActiveForm.Text = titulo;
+        }
+
+        private void Btn_OK_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.Yes;
+            this.Close();
+        }
+
+        private void Btn_Cancel_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.Cancel;
+            this.Close();
+        }
+    }
+}
