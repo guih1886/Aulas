a31c13eeef75f1562bb116c2fe2646940aff531e
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2024-02-19 22:08:29 -0300

    mouse events creation
    mouse events creation

---
+++ C#/WindowsForms/Curso/CursoWindowsForms/CursoWindowsForms/Forms/Form_MouseEventos.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CursoWindowsForms.Forms
+{
+    public partial class Form_MouseEventos : Form
+    {
+        public Form_MouseEventos()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn_Mouse_MouseEnter(object sender, EventArgs e)
+        {
+            Btn_Mouse.Text = "Mouse Enter";
+        }
+
+        private void Btn_Mouse_MouseLeave(object sender, EventArgs e)
+        {
+            Btn_Mouse.Text = "Mouse Leave";
+        }
+
+        private void Btn_Mouse_MouseDown(object sender, MouseEventArgs e)
+        {
+            
+            Btn_Mouse.BackColor = Color.Red;
+        }
+
+        private void Btn_Mouse_MouseUp(object sender, MouseEventArgs e)
+        {
+            Btn_Mouse.BackColor = Color.White;
+        }
+    }
+}
