0fe99d69b86de7fa09f19e653950f14c66b8f0c9
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2024-02-26 23:53:13 -0300

    Create Cep.cs

---
+++ C#/WindowsForms/Curso/CursoWindowsForms/CursoWindowsFormsBiblioteca/Classes/Cep.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+
+namespace CursoWindowsFormsBiblioteca.Classes
+{
+    public class Cep
+    {
+        public class Unit
+        {
+            public string Cep { get; set; }
+            public string Logradouro { get; set; }
+            public string Localidade { get; set; }
+            public string Complemento { get; set; }
+            public string Bairro { get; set; }
+            public string UF { get; set; }
+            public string Unidade { get; set; }
+            public string IBGE { get; set; }
+            public string GIA { get; set; }
+        }
+
+        public static Unit DesSerializeClassUnit(string json)
+        {
+            return JsonConvert.DeserializeObject<Unit>(json);
+        }
+    }
+}
