a567ba2dc642556356f462b99a827f5514d4ee59
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2024-03-04 17:38:37 -0300

    Create LocalDB.cs

---
+++ C#/WindowsForms/Curso/CursoWindowsForms/CursoWindowsFormsBiblioteca/Databases/LocalDB.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace CursoWindowsFormsBiblioteca.Databases
+{
+    public class LocalDB
+    {
+        public string connection;
+        public SqlConnection connectionDB;
+
+        public LocalDB()
+        {
+            try
+            {
+                this.connection = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\guilh\\Documents\\GitHub\\Aulas\\C#\\WindowsForms\\Curso\\CursoWindowsForms\\CursoWindowsFormsBiblioteca\\Databases\\Fichario.mdf;Integrated Security=True";
+                connectionDB = new SqlConnection(connection);
+                connectionDB.Open();
+            }
+            catch (Exception error)
+            {
+                throw new Exception(error.Message);
+            }
+        }
+
+        public string SQLComand(string comand)
+        {
+            try
+            {
+                var c = new SqlCommand(comand, connectionDB);
+                c.CommandTimeout = 0;
+
+                var reader = c.ExecuteReader();
+                return "";
+            }
+            catch (Exception error)
+            {
+                throw new Exception(error.Message);
+            }
+        }
+
+        public DataTable SQLQuery(string query)
+        {
+            DataTable dt = new DataTable();
+            try
+            {
+                SqlCommand c = new SqlCommand(query, connectionDB);
+                SqlDataReader reader = c.ExecuteReader();
+                dt.Load(reader);
+                return dt;
+            }
+            catch (Exception error)
+            {
+                throw new Exception(error.Message);
+            }
+        }
+
+        public void Close()
+        {
+            connectionDB.Close();
+        }
+    }
+}
