1816c21d25a2549604ec5f832cad1b2fd13181a6
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2024-03-15 18:43:34 -0300

    project creation
    project creation

---
+++ C#/alura-memoria-Aula-1/UsuarioLib/Program.cs
@@ -0,0 +1,22 @@
+﻿using UsuarioLib;
+
+List<Usuario> usuarios = new List<Usuario>();
+
+Usuario usuario =
+    new Usuario(
+        "Daniel",
+        "daniel@email.com",
+        new List<string>() { "12345678" });
+
+Usuario usuario2 =
+    new Usuario(
+        "Luis",
+        "luis@email.com",
+        new List<string>() { "87654321" });
+
+usuarios.Add(usuario);
+usuarios.Add(usuario2);
+
+usuarios.ForEach(usuario => usuario.ExibeTelefones());
+usuarios.ForEach(usuario => usuario.PadronizaTelefones());
+usuarios.ForEach(usuario => usuario.ExibeTelefones());
