1f743588de286a02f9c8515eb7b39ee57d867771
Guilherme Henrique <guilherme.oliveira@rtx.tec.br>
2024-10-25 22:26:22 -0300

    Revert "Aulas update"
    This reverts commit 390dbb76f16b85f44cc5dced2c79fb026563272c.

--- Dart/.idea/Classes/Pessoa.dart
+++ Dart/.idea/Classes/Pessoa.dart
@@ -1,36 +1,17 @@
 class Pessoa {
-  int _id;
-  String _nome;
-  int _idade;
-  bool _estaAutenticada;
+  String nome;
+  int idade;
+  bool estaAutenticada;
 
+  Pessoa(this.nome, this.idade, this.estaAutenticada);
 
-  Pessoa(this._nome, this._idade, this._estaAutenticada);
-
-  set nome(String value) {
-    _nome = value;
-  }
-
-  set idade(int value) {
-    _idade = value;
-  }
-
-  set estaAutenticada(bool value) {
-    _estaAutenticada = value;
-  }
-
-  String get nome => _nome;
-
-  int get idade => _idade;
-
-  bool get estaAutenticada => _estaAutenticada;
-
+  /* teste */
   Map<String, dynamic> toMap() {
     Map<String, dynamic> resultado = {};
 
-    resultado["nome"] = this._nome;
-    resultado["idade"] = this._idade;
-    resultado["estaAutenticada"] = this._estaAutenticada;
+    resultado["nome"] = this.nome;
+    resultado["idade"] = this.idade;
+    resultado["estaAutenticada"] = this.estaAutenticada;
 
     return resultado;
   }
