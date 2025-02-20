05feccb05254a0da95ba1396d3a5b6d5a4ef9a02
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2025-02-20 12:09:08 -0300

    aulas c# asyncs

---
+++ C#/dotnet_async_rec-master/dotnet_async/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+
+void LerArquivo()
+{
+    var conteudo = File.ReadAllText("voos.txt", Encoding.UTF8);
+    Console.WriteLine($"Conteudo: {conteudo}");
+}
+
+void ExibirRelatorio()
+{
+    Console.WriteLine("Exibindo relatorio de compra de passagens.");
+    Thread.Sleep(10000);
+}
+
+var threadLerArquivo = new Thread(() => LerArquivo());
+var threadExibirRelatorio = new Thread(() => ExibirRelatorio());
+
+void InicializarThreads()
+{
+    threadLerArquivo.Start();
+    threadExibirRelatorio.Start();
+}
+
+InicializarThreads();
+
+Console.WriteLine("Outras operações.");
