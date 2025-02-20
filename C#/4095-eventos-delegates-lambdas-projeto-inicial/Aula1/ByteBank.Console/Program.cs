899c126a0eca51a065a2485badd161db3fe6a0d6
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2025-02-20 13:13:15 -0300

    aulas c# delegates

--- C#/4095-eventos-delegates-lambdas-projeto-inicial/Aula1/ByteBank.Console/Program.cs
+++ C#/4095-eventos-delegates-lambdas-projeto-inicial/Aula1/ByteBank.Console/Program.cs
@@ -1,4 +1,8 @@
-﻿internal class Program
+﻿
+
+using System.Threading.Channels;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
@@ -29,6 +33,9 @@
         Console.WriteLine("2. Depositar valores");
         Console.WriteLine("3. Sacar valores");
         Console.WriteLine("4. Extrato");
+        Console.WriteLine("5. Depositar e obter saldo");
+        Console.WriteLine("6. Sacar e obter saldo");
+        Console.WriteLine("7. Depositar, aplicar na poupança e obter saldo");
         Console.WriteLine();
         Console.Write("Digite o número da opção desejada: ");
     }
@@ -48,6 +55,15 @@
                 break;
             case 4:
                 Extrato();
+                break;
+            case 5:
+                DepositarEObterSaldo();
+                break;
+            case 6:
+                SacarEObterSaldo();
+                break;
+            case 7:
+                DepositarAplicarEObterSaldo();
                 break;
 
             default:
@@ -76,12 +92,73 @@
         ExecutarTransacaoBancaria(caixaEletronico.Depositar, 25);
     }
 
+    private static void DepositarEObterSaldo()
+    {
+        //Delegates anônimos
+        TransacaoBancaria transacaoBancaria = delegate (decimal valor)
+        {
+            caixaEletronico.Depositar(valor);
+            caixaEletronico.Saldo();
+        };
+
+        transacaoBancaria(100);
+        transacaoBancaria(40);
+        transacaoBancaria(25);
+    }
+
+    private static void DepositarAplicarEObterSaldo()
+    {
+        //Executando delegates agrupados
+        TransacaoBancaria depositar = caixaEletronico.Depositar;
+        depositar(50);
+        Console.WriteLine("Delegate 'depositar' executado com sucesso!");
+        Console.WriteLine();
+
+        TransacaoBancaria aplicar = caixaEletronico.AplicarPoupanca;
+        aplicar(50);
+        Console.WriteLine("Delegate 'aplicar' executado com sucesso!");
+        Console.WriteLine();
+
+        TransacaoBancaria saldo = delegate (decimal valor)
+        {
+            caixaEletronico.Saldo();
+        };
+        saldo(50);
+        Console.WriteLine("Delegate 'saldo' executado com sucesso!");
+        Console.WriteLine();
+
+        //Delegates multicast são delegates combinados
+        TransacaoBancaria depositarAplicarSaldo = depositar + aplicar + saldo; //Operador faz uma sobrecarga
+        depositarAplicarSaldo(50);
+        Console.WriteLine("Delegate multicast 'depositarAplicarSaldo' executado com sucesso!");
+        Console.WriteLine();
+
+        //TransacaoBancaria aplicarSaldo = aplicar + saldo;
+        TransacaoBancaria aplicarSaldo = depositarAplicarSaldo - depositar;
+        aplicarSaldo(50);
+        Console.WriteLine("Delegate multicast 'aplicarSaldo' executado com sucesso!");
+        Console.WriteLine();
+    }
+
     private static void Sacar()
     {
         //caixaEletronico.Sacar(50);
         //caixaEletronico.Sacar(20);
         ExecutarTransacaoBancaria(caixaEletronico.Sacar, 50);
         ExecutarTransacaoBancaria(caixaEletronico.Sacar, 20);
+    }
+
+    private static void SacarEObterSaldo()
+    {
+        //Delegates anônimos
+        TransacaoBancaria transacao = delegate (decimal valor)
+        {
+            caixaEletronico.Sacar(valor);
+            caixaEletronico.Saldo();
+        };
+
+        transacao(50);
+        transacao(20);
     }
 
     private static void Extrato()
