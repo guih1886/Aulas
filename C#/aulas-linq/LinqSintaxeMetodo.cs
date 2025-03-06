7105af83f5c5f8f71bd975d3bf311ad903d122dd
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2025-03-06 12:19:05 -0300

    aluas c# linq

--- C#/aulas-linq/LinqSintaxeMetodo.cs
+++ C#/aulas-linq/LinqSintaxeMetodo.cs
@@ -1,11 +1,14 @@
 ﻿using SerenattoEnsaio.Dados;
 using SerenattoEnsaio.Modelos;
+using SerenattoPreGravacao.Dados;
 
 namespace Serenatto;
 
 static class LinqSintaxeMetodo
 {
     static List<Cliente> clientes = DadosClientes.GetClientes().ToList();
+    static List<Produto> produtosCardapio = DadosCardapio.GetProdutos();
+    static IEnumerable<int> dadosPedidos = DadosPedidos.QuantidadeItensPedidosPorDia.SelectMany(lista => lista);
 
     public static void ConsultaClientes()
     {
@@ -29,7 +32,55 @@ static class LinqSintaxeMetodo
         }
     }
 
+    public static void ConsultaProdutosPorNome()
+    {
+        Console.WriteLine("Dados produtos por nome");
+        var produtosPorNome = produtosCardapio.Select(p => p.Nome);
 
+        foreach (var item in produtosPorNome)
+        {
+            Console.WriteLine(item);
+        }
+    }
 
+    public static void ConsultaProdutosPorNomeEPreco()
+    {
+        Console.WriteLine("Dados produtos por nome e preço");
+        var produtosPorNome = produtosCardapio.Select(p => new
+        {
+            Nome = p.Nome,
+            Preco = p.Preco
+        });
 
+        foreach (var item in produtosPorNome)
+        {
+            Console.WriteLine($"{item.Nome} - R$ {item.Preco}");
+        }
+    }
+
+    public static void ProdutoPrecoCombo()
+    {
+        Console.WriteLine("Produtos preço combo LV4P3");
+
+        //Projeção anônima
+        var produtosPrecoCombo = produtosCardapio.Select(p => new
+        {
+            Nome = p.Nome,
+            PrecoCombo = p.Preco * 3
+        });
+
+        foreach (var item in produtosPrecoCombo)
+        {
+            Console.WriteLine($"{item.Nome} - R$ {item.PrecoCombo}");
+        }
+    }
+
+    public static void RelatorioQuantidadeProdutosPedidosMes()
+    {
+        Console.WriteLine("Produtos pedidos no mês");
+        foreach (var item in dadosPedidos)
+        {
+            Console.Write($"{item} ");
+        }
+    }
 }
--- C#/aulas-linq/Program.cs
+++ C#/aulas-linq/Program.cs
@@ -1,10 +1,19 @@
 ﻿using Serenatto;
 
 LinqSintaxeMetodo.ConsultaClientes();
+Console.WriteLine();
+LinqSintaxeMetodo.ConsultaProdutosPorNome();
+Console.WriteLine();
+LinqSintaxeMetodo.ConsultaProdutosPorNomeEPreco();
+Console.WriteLine();
+LinqSintaxeMetodo.ProdutoPrecoCombo();
+Console.WriteLine();
+LinqSintaxeMetodo.RelatorioQuantidadeProdutosPedidosMes();
+Console.WriteLine();
 LinqSintaxeMetodo.ConsultaClientesPorNome("Almeida");
+Console.WriteLine();
 
 Console.WriteLine("-----------------------------");
-
+Console.WriteLine();
 LinqSintaxeConsulta.ConsultaFormaPagamentos();
 
-Console.WriteLine("-----------------------------");
