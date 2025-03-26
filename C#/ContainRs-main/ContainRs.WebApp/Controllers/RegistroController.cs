5d371e1c9c7fdaa499a794052a3eada244b94763
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2025-03-26 15:11:54 -0300

    C# clean architecture update

--- C#/ContainRs-main/ContainRs.WebApp/Controllers/RegistroController.cs
+++ C#/ContainRs-main/ContainRs.WebApp/Controllers/RegistroController.cs
@@ -1,5 +1,7 @@
 ﻿using ContainRs.WebApp.Data;
 using ContainRs.WebApp.Models;
+using ContainRs.Domain.Models;
+using ContainRs.Application.UseCases;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ContainRs.WebApp.Controllers;
@@ -26,19 +28,9 @@ public class RegistroController : Controller
     {
         if (!ModelState.IsValid) return View("Index", form);
 
-        var cliente = new Cliente(form.Nome, form.Email, form.CPF)
-        {
-            Celular = form.Celular,
-            CEP = form.CEP,
-            Rua = form.Rua,
-            Numero = form.Numero,
-            Complemento = form.Complemento,
-            Bairro = form.Bairro,
-            Municipio = form.Municipio,
-            Estado = form.Estado
-        };
-        context.Clientes.Add(cliente);
-        await context.SaveChangesAsync();
+        var useCase = new RegistrarCliente(context,form.Nome, new Email(form.Email), form.CPF, form.Celular, form.CEP, form.Rua, form.Numero, form.Complemento, form.Bairro, form.Municipio, UfStringConverter.From(form.Estado));
+
+        await useCase.ExecutarAsync();
 
         return RedirectToAction("Sucesso");
     }
--- C#/ContainRs-main/ContainRs.WebApp/Data/AppDbContext.cs
+++ C#/ContainRs-main/ContainRs.WebApp/Data/AppDbContext.cs
@@ -1,15 +1,37 @@
-﻿using ContainRs.WebApp.Models;
+﻿using ContainRs.Application.Repositories;
+using ContainRs.Domain.Models;
 using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+using System.Linq;
 
 namespace ContainRs.WebApp.Data;
 
-public class AppDbContext : DbContext
+public class AppDbContext : DbContext, IClienteRepository
 {
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
     {
     }
 
     public DbSet<Cliente> Clientes { get; set; }
+
+    public async Task<Cliente> AddAsync(Cliente cliente)
+    {
+        await Clientes.AddAsync(cliente);
+        await SaveChangesAsync();
+        return cliente;
+    }
+
+    public async Task<IEnumerable<Cliente>> GetAsync(Expression<Func<Cliente, bool>>? filtro = null)
+    {
+        IQueryable<Cliente> queryClientes = this.Clientes;
+        if (filtro != null)
+        {
+            queryClientes = queryClientes.Where(filtro);
+        }
+        return await queryClientes
+            .AsNoTracking()
+            .ToListAsync();
+    }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
@@ -21,7 +43,17 @@ public class AppDbContext : DbContext
         modelBuilder.Entity<Cliente>()
             .Property(c => c.Nome).IsRequired();
         modelBuilder.Entity<Cliente>()
-            .Property(c => c.Email).IsRequired();
+            .OwnsOne(c => c.Email, cfg =>
+            {
+                cfg.Property(e => e.Value)
+                    .HasColumnName("Email")
+                    .IsRequired();
+            });
+
+        modelBuilder.Entity<Cliente>()
+            .Property(c => c.Estado)
+            .HasConversion<string>();
+
         modelBuilder.Entity<Cliente>()
             .Property(c => c.CPF).IsRequired();
     }
